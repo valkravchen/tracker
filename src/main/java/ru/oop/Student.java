1d13970721a49698c10d04eb77235cf86307f0bf
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-01-30 20:32:51 +0300

    7.3. Вызов метода объекта (практика)

--- src/main/java/ru/oop/Student.java
+++ src/main/java/ru/oop/Student.java
@@ -2,6 +2,10 @@ package ru.oop;
 
 public class Student {
 
+    public void sing() {
+        System.out.println("I believe I can fly");
+    }
+
     public void music() {
         System.out.println("Tra tra tra");
     }
@@ -11,5 +15,8 @@ public class Student {
         petya.music();
         petya.music();
         petya.music();
+        petya.sing();
+        petya.sing();
+        petya.sing();
     }
 }
