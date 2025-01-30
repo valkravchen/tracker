048f2ad3815a9ae490987524a81e9ce89a682149
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-01-30 19:26:38 +0300

    7.3. Вызов метода объекта

---
+++ src/main/java/ru/oop/Student.java
@@ -0,0 +1,12 @@
+package ru.oop;
+
+public class Student {
+
+    public void music() {
+        System.out.println("Tra tra tra");
+    }
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+    }
+}
