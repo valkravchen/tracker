337866b5eb300ccfa938649bf2e4887c0be6ddf5
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-01-31 19:24:46 +0300

    7.4. Вызов метода с аргументами (теория)

--- src/main/java/ru/oop/Student.java
+++ src/main/java/ru/oop/Student.java
@@ -6,17 +6,13 @@ public class Student {
         System.out.println("I believe I can fly");
     }
 
-    public void music() {
-        System.out.println("Tra tra tra");
+    public void music(String lyrics) {
+        System.out.println("I can sing a song: " + lyrics);
     }
 
     public static void main(String[] args) {
         Student petya = new Student();
-        petya.music();
-        petya.music();
-        petya.music();
-        petya.sing();
-        petya.sing();
-        petya.sing();
+        String song = "I believe, I can fly";
+        petya.music(song);
     }
 }
