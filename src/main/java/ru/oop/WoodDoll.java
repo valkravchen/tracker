c39ff3a9fc653bf31761ddc53901c41b0eef6a74
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-02 15:29:40 +0300

    7.11. Класс, использующий сам себя (теория)

---
+++ src/main/java/ru/oop/WoodDoll.java
@@ -0,0 +1,26 @@
+package ru.oop;
+
+public class WoodDoll {
+    private WoodDoll child;
+    private String name;
+
+    public WoodDoll(WoodDoll child, String name) {
+        this.child = child;
+        this.name = name;
+    }
+
+    public void about() {
+        System.out.println("Hello, my name is " + name);
+        if (child != null) {
+            System.out.println("I have a child. Let's I call her ...");
+            child.about();
+        } else {
+            System.out.println("I don't have a child.");
+        }
+    }
+
+    public static void main(String[] args) {
+        WoodDoll little = new WoodDoll(null, "Little doll");
+        little.about();
+    }
+}
