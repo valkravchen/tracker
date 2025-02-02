c6f7b52cf31f0ea9a69694f4288e58e72e735841
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-02 12:52:38 +0300

    7.10. Переменные и ссылочные типы данных.

--- src/main/java/ru/oop/Tamagotchi.java
+++ src/main/java/ru/oop/Tamagotchi.java
@@ -3,6 +3,12 @@ package ru.oop;
 public class Tamagotchi {
     public static void main(String[] args) {
         int weight = 100;
-        System.out.println(weight);
+        int weightCopy = weight;
+        System.out.println("Weight: " + weight);
+        System.out.println("Copy of weight: " + weightCopy);
+        System.out.println();
+        weightCopy += 10;
+        System.out.println("Weight: " + weight);
+        System.out.println("Copy of weight: " + weightCopy);
     }
 }
