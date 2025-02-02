7b2a90cb41e7aca9d5969f17ad96bde6ad4b7743
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-02 07:53:39 +0300

    7.9. Перегрузка конструктора (теория)

--- src/main/java/ru/oop/Computer.java
+++ src/main/java/ru/oop/Computer.java
@@ -2,21 +2,7 @@ package ru.oop;
 
 public class Computer {
 
-    private boolean multiMonitor;
-    private int ssd;
-    private String cpu;
-
-    public Computer() {
-    }
-
-    public void printInfo() {
-        System.out.println("Много мониторов: " + multiMonitor);
-        System.out.println("SSD: " + ssd + " GB");
-        System.out.println("Модель CPU: " + cpu);
-    }
-
     public static void main(String[] args) {
-       Computer computer = new Computer();
-       computer.printInfo();
+       Computer first = new Computer();
     }
 }
