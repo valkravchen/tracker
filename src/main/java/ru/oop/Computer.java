f9ccb0bc3b9e5b736c7869df67642bbdb1257520
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-02 09:22:02 +0300

    7.9. Перегрузка конструктора (теория)

--- src/main/java/ru/oop/Computer.java
+++ src/main/java/ru/oop/Computer.java
@@ -20,6 +20,18 @@ public class Computer {
         this.cpu = cpu;
     }
 
+    public Computer(int ssd, String cpu, boolean multiMonitor) {
+        this.ssd = ssd;
+        this.cpu = cpu;
+        this.multiMonitor = multiMonitor;
+    }
+
+    public Computer(boolean multiMonitor, double ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = (int) ssd;
+        this.cpu = cpu;
+    }
+
     public void printInfo() {
         System.out.println("Много мониторов: " + multiMonitor);
         System.out.println("SSD: " + ssd + " GB");
@@ -31,8 +43,12 @@ public class Computer {
        Computer first = new Computer();
        Computer second = new Computer(true, 500, "Intel Core I7-10700K");
        Computer third = new Computer(256, "AMD Ryzen 5 3600");
+       Computer fourth = new Computer(256, "Intel Core I7-10700K", true);
+       Computer fifth = new Computer(true, 512.0, "AMD Ryzen 7 3700X");
        first.printInfo();
        second.printInfo();
        third.printInfo();
+       fourth.printInfo();
+       fifth.printInfo();
     }
 }
