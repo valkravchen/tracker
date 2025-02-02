6845ed216bfa66efdd6e4d374c09e864139d6388
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-02 06:09:04 +0300

    7.8. Конструктор (теория)

--- src/main/java/ru/oop/Computer.java
+++ src/main/java/ru/oop/Computer.java
@@ -6,10 +6,7 @@ public class Computer {
     private int ssd;
     private String cpu;
 
-    public Computer(boolean multiMonitor, int ssd, String cpu) {
-       this.multiMonitor = multiMonitor;
-       this.ssd = ssd;
-       this.cpu = cpu;
+    public Computer() {
     }
 
     public void printInfo() {
@@ -19,9 +16,7 @@ public class Computer {
     }
 
     public static void main(String[] args) {
-       Computer computer = new Computer(true, 500, "Intel Core I7-10700K");
-       Computer comp = new Computer(true, 256, "AMD Ryzen 5 3600");
+       Computer computer = new Computer();
        computer.printInfo();
-       comp.printInfo();
     }
 }
