529d65209b05e8976b0b23eb860192d797979460
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-05 18:45:35 +0300

    7.11. Класс, использующий сам себя

--- src/main/java/ru/oop/Battery.java
+++ src/main/java/ru/oop/Battery.java
@@ -12,13 +12,15 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-
+        another.load = this.load + another.load;
+        this.load = 0;
     }
 
     public static void main(String[] args) {
         Battery first = new Battery(70);
         Battery second = new Battery(30);
-        System.out.println("First." + first.about());
-        System.out.println("Second." + second.about());
+        first.exchange(second);
+        System.out.println("First. " + first.about());
+        System.out.println("Second. " + second.about());
     }
 }
