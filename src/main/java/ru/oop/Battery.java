86fd5dbca714549a140e6d682c69e0d4236d1928
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-05 18:15:02 +0300

    7.11. Класс, использующий сам себя

---
+++ src/main/java/ru/oop/Battery.java
@@ -0,0 +1,24 @@
+package ru.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public String about() {
+        return "My charge: " + load + "%";
+    }
+
+    public void exchange(Battery another) {
+
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(70);
+        Battery second = new Battery(30);
+        System.out.println("First." + first.about());
+        System.out.println("Second." + second.about());
+    }
+}
