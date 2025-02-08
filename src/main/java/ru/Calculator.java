156b26ab8643ab0069df88246533213e269ad969
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-08 15:01:37 +0300

    7.15. Перегрузить метод max для трёх чисел

---
+++ src/main/java/ru/Calculator.java
@@ -0,0 +1,12 @@
+package ru;
+
+public class Calculator {
+
+    public double add(double first, double second){
+        return first + second;
+    }
+
+    public double add(double first, double second, double third) {
+        return first + second + third;
+    }
+}
