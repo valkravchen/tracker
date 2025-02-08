33d75fc0a89c6472a314d3c2094a30f7d0301d63
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-08 15:50:09 +0300

    7.15. Перегрузить метод max для трёх чисел

---
+++ src/main/java/ru/oop/OverloadRunner.java
@@ -0,0 +1,15 @@
+package ru.oop;
+
+public class OverloadRunner {
+    public void print(String name, int age, boolean allow) {
+        System.out.println("Your name: " + name);
+        System.out.println("Your age: " + age);
+        System.out.println("Your allow: " + allow);
+    }
+
+    public void print(String name, double age, boolean allow) {
+        System.out.println("Your name: " + name);
+        System.out.println("Your age: " + age);
+        System.out.println("Your allow: " + allow);
+    }
+}
