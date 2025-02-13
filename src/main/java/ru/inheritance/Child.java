e715a84c99cd2b640d417a1d940a7ebd23635e5f
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-13 19:35:38 +0300

    8.2. Вызов конструктора родительского класса с помощью ключевого слова super.

---
+++ src/main/java/ru/inheritance/Child.java
@@ -0,0 +1,11 @@
+package ru.inheritance;
+
+public class Child extends Parent {
+
+    private String patronymic;
+
+    public Child(String name, int age, String patronymic) {
+        super(name, age);
+        this.patronymic = patronymic;
+    }
+}
---
+++ src/main/java/ru/inheritance/Parent.java
@@ -0,0 +1,12 @@
+package ru.inheritance;
+
+public class Parent {
+
+    private String name;
+    private int age;
+
+    Parent(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
