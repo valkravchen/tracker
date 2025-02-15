a2f2f136433b056e7c826afad28cd9a64a59dbff
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-15 06:20:49 +0300

    8.3. Переопределение метода (теория)

--- src/main/java/ru/inheritance/HtmlReport.java
+++ src/main/java/ru/inheritance/HtmlReport.java
@@ -1,4 +1,10 @@
 package ru.inheritance;
 
 public class HtmlReport extends TextReport{
+
+    public String generate(String name, String body) {
+        return "<h1>" + name + "</h1>"
+                + "<br/>"
+                + "<span>" + body + "</span>";
+    }
 }
