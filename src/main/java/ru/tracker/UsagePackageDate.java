7b0863727719cbb85f5229947fa1869a72e9732c
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-15 16:59:46 +0300

    8.7. Date. Отображение даты

---
+++ src/main/java/ru/tracker/UsagePackageDate.java
@@ -0,0 +1,10 @@
+package ru.tracker;
+
+import java.time.LocalDate;
+
+public class UsagePackageDate {
+    public static void main(String[] args) {
+        LocalDate currentDate = LocalDate.now();
+        System.out.println("Текущая дата: " + currentDate);
+    }
+}
