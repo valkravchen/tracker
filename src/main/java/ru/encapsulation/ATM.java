744c9d82a1c21c3bbe0df44df2b6122082bc8b89
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-16 11:29:43 +0300

    9.1. Что такое инкапсуляция

--- src/main/java/ru/encapsulation/ATM.java
+++ src/main/java/ru/encapsulation/ATM.java
@@ -4,6 +4,6 @@ public class ATM {
     public static void main(String[] args) {
         Account account = new Account();
         account.addMoney(100);
-        account.addMoney(50);
+        account.addMoney(505);
     }
 }
