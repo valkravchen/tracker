45bd04ebbbd3f88d899378cf8b5aad9b5f345615
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-16 10:14:45 +0300

    9.1. Что такое инкапсуляция

---
+++ src/main/java/ru/encapsulation/Account.java
@@ -0,0 +1,11 @@
+package ru.encapsulation;
+
+public class Account {
+    private int balance;
+
+    public void addMoney(int money) {
+        if (money > 0) {
+            balance += money;
+        }
+    }
+}
