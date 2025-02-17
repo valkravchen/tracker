94bb78fd06bb0d1a5ae9b1592bdb1d352946e39f
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-17 18:54:22 +0300

    9.4. Массив с пустыми ячейками

---
+++ src/main/java/ru/pojo/Shop.java
@@ -0,0 +1,15 @@
+package ru.pojo;
+
+public class Shop {
+    public static void main(String[] args) {
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bred", 4);
+        products[2] = new Product("Egg", 19);
+        for (Product product : products) {
+            if (product != null) {
+                System.out.println(product.getName());
+            }
+        }
+    }
+}
