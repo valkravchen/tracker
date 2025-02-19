fa2ed8b827be32d3718fcd5361c16adcb17fbd86
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-19 17:48:31 +0300

    9.6. Сравнение моделей. Метод equals

--- src/main/java/ru/pojo/ProductContains.java
+++ src/main/java/ru/pojo/ProductContains.java
@@ -4,7 +4,7 @@ public class ProductContains {
     public static void main(String[] args) {
         Product first = new Product("Milk", 100);
         Product second = new Product("Milk", 100);
-        boolean equal = first == second;
+        boolean equal = first.equals(second);
         System.out.println(equal);
     }
 }
