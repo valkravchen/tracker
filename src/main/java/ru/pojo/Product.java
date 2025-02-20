34de674b85989b63b694bc947fe0198d687a6057
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-20 12:29:09 +0300

    9.6. Сравнение моделей. Метод equals

--- src/main/java/ru/pojo/Product.java
+++ src/main/java/ru/pojo/Product.java
@@ -1,5 +1,7 @@
 package ru.pojo;
 
+import java.util.Objects;
+
 public class Product {
     private String name;
     private int count;
@@ -11,6 +13,23 @@ public class Product {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return count == product.count && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, count);
     }
 
     public void setName(String name) {
