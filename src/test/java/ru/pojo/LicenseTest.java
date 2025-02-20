958e0b2f6ead068fef9f05b57e39eda46119f9f6
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-20 12:39:59 +0300

    9.6. Сравнение моделей. Метод equals (практика)

---
+++ src/test/java/ru/pojo/LicenseTest.java
@@ -0,0 +1,16 @@
+package ru.pojo;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LicenseTest {
+
+    @Test
+    void eqName() {
+        License first = new License();
+        first.setCode("audio");
+        License second = new License();
+        second.setCode("audio");
+        assertThat(first).isEqualTo(second);
+    }
+}
