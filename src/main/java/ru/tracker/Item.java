ac7e7a7bdf2894c477b65974aa64ef877eec35e4
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-23 19:25:47 +0300

    9.7. Tracker - хранилище

--- src/main/java/ru/tracker/Item.java
+++ src/main/java/ru/tracker/Item.java
@@ -1,7 +1,6 @@
 package ru.tracker;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 public class Item {
     private int id;
@@ -13,23 +12,6 @@ public class Item {
 
     public Item(String name) {
         this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Item item = (Item) o;
-        return id == item.id && Objects.equals(name, item.name) && Objects.equals(created, item.created);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, created);
     }
 
     public Item(int id, String name) {
