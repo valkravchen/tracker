0e4a5ba8e693b2a38164a9f8180bcc9d2059e07c
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-23 13:47:01 +0300

    9.7. Tracker - хранилище

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -1,5 +1,8 @@
 package ru.tracker;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -11,15 +14,49 @@ public class Tracker {
         return item;
     }
 
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Tracker tracker = (Tracker) o;
+        return ids == tracker.ids && size == tracker.size && Arrays.equals(items, tracker.items);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Arrays.hashCode(items), ids, size);
+    }
+
+    public Item[] findByName(String key) {
+       Item[] result = new Item[size];
+       int count = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item != null && key.equals(item.getName())) {
+                result[count++] = item;
+            }
+        }
+        return Arrays.copyOf(result, count);
+    }
+
     public Item findById(int id) {
-        Item rsl = null;
+        Item result = null;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
             if (item.getId() == id) {
-                rsl = item;
+                result = item;
                 break;
             }
         }
-        return rsl;
+        return result;
     }
 }
+
