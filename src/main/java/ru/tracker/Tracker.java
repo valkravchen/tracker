7eb265409da4112ceece961cb150066b4ba1d7e3
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-25 10:15:22 +0300

    9.9. Метод удаления заявки

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -53,5 +53,15 @@ public class Tracker {
         }
         return false;
     }
+
+    public void delete(int id) {
+        int index = indexOf(id);
+        if (index == -1) {
+            return;
+        }
+        System.arraycopy(items, index + 1, items, index, size - index - 1);
+        items[size - 1] = null;
+        size--;
+    }
 }
 
