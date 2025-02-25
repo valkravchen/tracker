5857a2c0f4a8f1be82059ed95fa69d9e09cf2696
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-25 09:46:57 +0300

    9.9. Метод удаления заявки (теория)

--- src/main/java/ru/tracker/DropArray.java
+++ src/main/java/ru/tracker/DropArray.java
@@ -4,18 +4,9 @@ import java.util.Arrays;
 
 public class DropArray {
     public static void main(String[] args) {
-        String[] names = {"Petr", null, "Ivan", "Stepan", null};
-        String[] result = new String[names.length];
-        int size = 0;
-        for (String name : names) {
-            if (name != null) {
-                result[size] = name;
-                size++;
-            }
-        }
-        result = Arrays.copyOf(result, size);
-        for (String s : result) {
-            System.out.println(s);
-        }
+        String[] names = {"Petr", null, "Ivan", "Stepan", "Fedor"};
+        System.arraycopy(names, 2, names, 1, 3);
+        System.out.println(Arrays.toString(names));
     }
 }
+
---
+++ src/main/java/ru/tracker/Main.java
@@ -0,0 +1,34 @@
+package ru.tracker;
+
+public class Main {
+    public static void main(String[] args) {
+        Tracker tracker = new Tracker();
+
+        Item item1 = new Item("Item 1");
+        Item item2 = new Item("Item 2");
+        Item item3 = new Item("Item 3");
+
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+
+        for (Item item : tracker.findAll()) {
+            System.out.println("ID: " + item.getId() + ", Name: " + item.getName());
+        }
+
+        Item item4 = new Item("Замена Item 1");
+        boolean replaced = tracker.replace(1, item4);
+
+
+        for (Item item : tracker.findAll()) {
+            System.out.println("ID: " + item.getId() + ", Name: " + item.getName());
+        }
+
+        for (Item item : tracker.findByName("Item 2")) {
+            System.out.println("ID: " + item.getId() + ", Name: " + item.getName());
+        }
+
+        Item item5 = tracker.findById(1);
+        System.out.println(item5.getName());
+    }
+}
