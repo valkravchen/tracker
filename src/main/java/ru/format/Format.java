6ea9e7474475668bb5f012a92a3e8d6da9e1a6ad
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-26 18:57:05 +0300

    9.11. Форматирование данных format и printf

--- src/main/java/ru/format/Format.java
+++ src/main/java/ru/format/Format.java
@@ -3,7 +3,7 @@ package ru.format;
 public class Format {
 
     public static void getGreeting(String name) {
-        System.out.println(String.format("Аргумент передан? %b", 55));
+        System.out.println(String.format("Ja%ca", 'v'));
     }
 
     public static void main(String[] args) {
