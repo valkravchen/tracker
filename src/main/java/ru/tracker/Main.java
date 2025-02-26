a1199c1c1661b31bd51b9910a422cc538c3dacf3
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-26 12:40:32 +0300

    Повторенье — мать ученья.

--- src/main/java/ru/tracker/Main.java
+++ src/main/java/ru/tracker/Main.java
@@ -2,32 +2,28 @@ package ru.tracker;
 
 public class Main {
     public static void main(String[] args) {
+
         Tracker tracker = new Tracker();
 
-        Item item1 = new Item("Item 1");
-        Item item2 = new Item("Item 2");
-        Item item3 = new Item("Item 3");
+        System.out.println("Добавляем объекты:");
 
+        Item item1 = new Item("Первый");
+        Item item2 = new Item();
+        Item item3 = new Item("Третий");
         tracker.add(item1);
         tracker.add(item2);
         tracker.add(item3);
 
+        System.out.println("Добавлено: ID = " + item1.getId() + ", Name = " + item1.getName());
+        System.out.println("Добавлено: ID = " + item2.getId() + ", Name = " + item2.getName());
+        System.out.println("Добавлено: ID = " + item3.getId() + ", Name = " + item3.getName());
+
+
+        System.out.println("\nВсе элементы в трекере:");
         for (Item item : tracker.findAll()) {
             System.out.println("ID: " + item.getId() + ", Name: " + item.getName());
         }
 
-        Item item4 = new Item("Замена Item 1");
-        boolean replaced = tracker.replace(1, item4);
-
-        for (Item item : tracker.findAll()) {
-            System.out.println("ID: " + item.getId() + ", Name: " + item.getName());
-        }
-
-        for (Item item : tracker.findByName("Item 2")) {
-            System.out.println("ID: " + item.getId() + ", Name: " + item.getName());
-        }
-
-        Item item5 = tracker.findById(1);
-        System.out.println(item5.getName());
+        System.out.println("\nПроверяем поиск по ID");
     }
 }
