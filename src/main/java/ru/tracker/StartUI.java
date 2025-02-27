088a3206fcbf82222a28875562aec8efb9a8d3b8
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-27 11:29:13 +0300

    9.12. toString(). Практика.

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -2,7 +2,7 @@ package ru.tracker;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item(1, "Первая заявка");
+        Item item = new Item(1, "Заявка 1");
         System.out.println(item);
     }
 }
