85fad06ea5c75fceedf4498b73e50262e8d3a3a2
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-27 15:47:55 +0300

    9.13. record

--- src/main/java/ru/record/PersonMain.java
+++ src/main/java/ru/record/PersonMain.java
@@ -1,4 +1,14 @@
 package ru.record;
 
 public class PersonMain {
+    public static void main(String[] args) {
+        Person person = new Person("Иван", 30);
+        System.out.println("Имя: " + person.getName() + ", возраст: " + person.getAge());
+        System.out.println(person);
+        PersonRecord record = new PersonRecord("Иван", 30);
+        System.out.println("Имя: " + record.name() + ", возраст: " + record.age());
+        System.out.println(record);
+        PersonRecord record1 = new PersonRecord("Иван", 30);
+        System.out.println(record.equals(record1));
+    }
 }
--- src/main/java/ru/record/PersonRecord.java
+++ src/main/java/ru/record/PersonRecord.java
@@ -1,4 +1,11 @@
 package ru.record;
 
 public record PersonRecord(String name, int age) {
+    @Override
+    public String toString() {
+        return "PersonRecord{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
