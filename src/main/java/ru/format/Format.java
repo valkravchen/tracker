dc8715c35a7cddb8de89514f6585b4a1639cad63
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-27 07:35:39 +0300

    9.11. Форматирование данных format и printf

--- src/main/java/ru/format/Format.java
+++ src/main/java/ru/format/Format.java
@@ -5,15 +5,12 @@ import java.util.Calendar;
 public class Format {
 
     public static void getGreeting(String name) {
-        System.out.format("Строка1%nСтрока2");
+        System.out.println(String.format("Задаем число 0.99 - %.2f", 0.99));
     }
 
     public static void getGreetingAndTime(String name) {
         Calendar instance = Calendar.getInstance();
         System.out.println(String.format("Привет, %s! Текущая дата: %tF, текущее время: %tT", name, instance, instance));
     }
-
-    public static void main(String[] args) {
-        getGreetingAndTime("Игорь");
-    }
 }
+
