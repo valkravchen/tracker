84593126c669cf989cfcee31ba7390af6275dc41
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-27 16:18:05 +0300

    9.13. record

--- src/main/java/ru/record/PersonRecord.java
+++ src/main/java/ru/record/PersonRecord.java
@@ -1,11 +1,10 @@
 package ru.record;
 
 public record PersonRecord(String name, int age) {
-    @Override
-    public String toString() {
-        return "PersonRecord{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
+    public PersonRecord {
+        if (age > 101) {
+            throw new IllegalArgumentException("Возраст должен быть менее 101");
+        }
     }
 }
+
