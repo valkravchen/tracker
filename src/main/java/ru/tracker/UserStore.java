28c3f1cb0017d705acd50191241e56fc46e1ad48
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-02-27 08:57:10 +0300

    9.12. toString()

--- src/main/java/ru/tracker/UserStore.java
+++ src/main/java/ru/tracker/UserStore.java
@@ -3,6 +3,9 @@ package ru.tracker;
 public class UserStore {
     public static void main(String[] args) {
         User user = new User(1, "root", "root");
-        System.out.println(user);
+        System.out.println("User: "
+                + user.getId() + " "
+                + user.getUsername() + " "
+                + user.getPassword());
     }
 }
