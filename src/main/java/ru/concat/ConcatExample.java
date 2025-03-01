2f1c251cffbb3676861f458ef4a7f492d588ac3f
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-01 18:11:04 +0300

    9.15. StringBuilder, StringJoiner, StringBuffer

--- src/main/java/ru/concat/ConcatExample.java
+++ src/main/java/ru/concat/ConcatExample.java
@@ -2,7 +2,7 @@ package ru.concat;
 
 public class ConcatExample {
     public static void main(String[] args) {
-        String str = "abc".concat("def").concat("ghi").concat("jkl");
+        String str = String.join(" ", "abc", "def", "ghi");
         System.out.println(str);
     }
 }
