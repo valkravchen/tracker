4d3f5631a1e3589389d76241a3bbe26db34b7d86
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-01 10:13:44 +0300

    9.14. Enum

--- src/main/java/ru/enumeration/CarService.java
+++ src/main/java/ru/enumeration/CarService.java
@@ -2,10 +2,11 @@ package ru.enumeration;
 
 public class CarService {
     public static void main(String[] args) {
-        Order order = new Order(1, "Mercedes-Benz GLS", Status.IN_WORK);
-        System.out.println("Заказ-наряд №" + order.getNumber() + " на автомобиль " + order.getCar()
-                + ", статус ремонта: " + order.getStatus().getInfo()
-                + ", подробности: " + order.getStatus().getMessage());
+        Status[] statuses = Status.values();
+        for (Status status : statuses) {
+            System.out.println("Название статуса: " + status.name()
+                    + ", порядковый номер статуса: " + status.ordinal());
+        }
     }
 }
 
