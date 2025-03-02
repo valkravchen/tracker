a891de9aad8c81b93fae483e63eefe6d80154dbd
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-02 16:20:35 +0300

    9.16. varargs

--- src/main/java/ru/varargs/VarArgs.java
+++ src/main/java/ru/varargs/VarArgs.java
@@ -7,14 +7,12 @@ public class VarArgs {
         for (String s : strings) {
             builder.append(s);
         }
-        return builder.toString();
+        return builder + " количество соединенных элементов: " + strings.length;
     }
 
     public static void main(String[] args) {
-        System.out.println(text());
-        System.out.println(text("aaa"));
         System.out.println(text("aaa", "bbb", "ccc"));
-        System.out.println(text("aaa", "bbb", "ccc", "ddd", "eee"));
     }
 }
+
 
