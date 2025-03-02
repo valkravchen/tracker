6a337ad290e4daecdc5e70b41451b33be8140259
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-02 19:00:17 +0300

    9.16. varargs

--- src/main/java/ru/varargs/VarArgs.java
+++ src/main/java/ru/varargs/VarArgs.java
@@ -1,29 +1,14 @@
 package ru.varargs;
 
 public class VarArgs {
-
-    public static String text(String... strings) {
-        StringBuilder builder = new StringBuilder();
-        for (String s : strings) {
-            builder.append(s);
+    public static void printAll(Object...items) {
+        for (Object item : items) {
+            System.out.println(item.getClass().getSimpleName() + ": " + item);
         }
-        return builder.toString();
-    }
-
-    public static String text(int x, String... strings) {
-        StringBuilder builder = new StringBuilder();
-        builder.append(x);
-        for (String s : strings) {
-            builder.append(s);
-        }
-        return builder.toString();
     }
 
     public static void main(String[] args) {
-        System.out.println(text());
-        System.out.println(text("aaa"));
-        System.out.println(text("aaa", "bbb", "ccc"));
-        System.out.println(text(30, "aaa", "bbb"));
+        printAll("Hello", 3, 3.333, true);
     }
 }
 
