34255802cceb7344987d95df11eefdfe2f1b9ff5
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-02 15:58:49 +0300

    9.16. varargs

--- src/main/java/ru/concat/StringJoinerExample.java
+++ src/main/java/ru/concat/StringJoinerExample.java
@@ -1,17 +1,13 @@
 package ru.concat;
 
-
 public class StringJoinerExample {
     public static void main(String[] args) {
-        // Тест StringBuffer
         long startBuffer = System.currentTimeMillis();
         StringBuffer buffer = new StringBuffer();
         for (int i = 0; i < 1_000_000; i++) {
             buffer.append(i);
         }
         System.out.println("StringBuffer: " + (System.currentTimeMillis() - startBuffer) + " мс");
-
-        // Тест StringBuilder
         long startBuilder = System.currentTimeMillis();
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < 1_000_000; i++) {
