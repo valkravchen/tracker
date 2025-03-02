279eff831ba3f3e4bf6166f76e6daba26bdc7613
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-02 15:56:10 +0300

    9.16. varargs

---
+++ src/main/java/ru/varargs/VarArgs.java
@@ -0,0 +1,17 @@
+package ru.varargs;
+
+public class VarArgs {
+
+    public String text(String a) {
+        return "";
+    }
+
+    public String text(String a, String b) {
+        return "";
+    }
+
+    public String text(String a, String b, String c) {
+        return "";
+    }
+}
+
