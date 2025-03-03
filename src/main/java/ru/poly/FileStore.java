61cf62889d7150056a7c7239a060fec831ae7629
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-03 18:21:29 +0300

    10.1. Полиморфизм

--- src/main/java/ru/poly/FileStore.java
+++ src/main/java/ru/poly/FileStore.java
@@ -1,4 +1,19 @@
 package ru.poly;
 
 public class FileStore {
+    private String path;
+
+    public FileStore() {
+    }
+
+    public FileStore(String path) {
+        this.path = path;
+    }
+
+    public void save(String data) {
+    }
+
+    public String read() {
+        return null;
+    }
 }
