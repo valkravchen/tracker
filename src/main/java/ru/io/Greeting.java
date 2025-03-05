b26d351ca0fcd389721c1e236a9c07b36bad4017
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-05 18:23:56 +0300

    10.2. Класс Scanner

--- src/main/java/ru/io/Greeting.java
+++ src/main/java/ru/io/Greeting.java
@@ -1,4 +1,12 @@
 package ru.io;
 
+import java.util.Scanner;
+
 public class Greeting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Добро пожаловать. Как Вас зовут? ");
+        String name = input.nextLine();
+        System.out.println(name + ", рад Вас видеть!");
+    }
 }
