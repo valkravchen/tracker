c25f7a04dabd1754a7c8ef9f96a8e1bd6f57b18e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-08 07:56:00 +0300

    10.7. StartUI. Изменение заявки

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -19,12 +19,12 @@ public class StartUI {
             } else if (select == 1) {
                 System.out.println("=== Вывод всех заявок ===");
                 Item[] items = tracker.findAll();
-                if (items.length == 0) {
-                    System.out.println("Хранилище ещё не содержит заявок");
-                } else {
+                if (items.length > 0) {
                     for (Item item : items) {
                         System.out.println(item);
                     }
+                } else {
+                    System.out.println("Хранилище еще не содержит заявок");
                 }
             } else if (select == 6) {
                 run = false;
