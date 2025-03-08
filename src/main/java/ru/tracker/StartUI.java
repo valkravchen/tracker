b9e67b53201694a5a397155d0552fb525be317d3
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-08 07:57:44 +0300

    10.6. StartUI. Вывод всех заявок

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -24,7 +24,7 @@ public class StartUI {
                         System.out.println(item);
                     }
                 } else {
-                    System.out.println("Хранилище еще не содержит заявок");
+                    System.out.println("Хранлище еще не содержит заявок");
                 }
             } else if (select == 6) {
                 run = false;
