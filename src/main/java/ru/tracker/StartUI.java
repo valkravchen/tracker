649d790d863a117442600d193424af369f1a0ec3
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-08 17:57:54 +0300

    10.8. StartUI. Удаление заявки

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -42,15 +42,11 @@ public class StartUI {
                 System.out.println("=== Удаление заявки ===");
                 System.out.print("Введите id заявки: ");
                 int id = Integer.parseInt(scanner.nextLine());
-                int sizeBefore = tracker.findAll().length;
-                tracker.delete(id);
-                int sizeAfter = tracker.findAll().length;
-                if (sizeBefore - 1 == sizeAfter) {
+                if (tracker.delete(id)) {
                     System.out.println("Заявка успешно удалена.");
                 } else {
                     System.out.println("Ошибка: заявка с id " + id + " не найдена.");
                 }
-
             } else if (select == 6) {
                 run = false;
             }
