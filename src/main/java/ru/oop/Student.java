796c685f100a89f1045aff25137aab771345df23
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-14 19:07:38 +0300

    10.14. Приведение типов

--- src/main/java/ru/oop/Student.java
+++ src/main/java/ru/oop/Student.java
@@ -1,18 +1,11 @@
 package ru.oop;
 
 public class Student {
-
     public void sing() {
         System.out.println("I believe I can fly");
     }
 
     public void music(String lyrics) {
         System.out.println("I can sing a song: " + lyrics);
-    }
-
-    public static void main(String[] args) {
-        Student petya = new Student();
-        String song = "I believe, I can fly";
-        petya.music(song);
     }
 }
