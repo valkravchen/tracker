07e69ef1e35ec133253f427c5232e4657fb9bf8e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-15 10:14:21 +0300

    10.15. Зачем нужно приведение типов

---
+++ src/main/java/ru/cast/DomesticAnimal.java
@@ -0,0 +1,7 @@
+package ru.cast;
+
+public class DomesticAnimal extends Animal {
+    public void liveOnFarm() {
+        System.out.println(nameClass + " живет на ферме, приносит пользу.");
+    }
+}
