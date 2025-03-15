bafc6c80cef0676744fa96f0f24bb7b79bc8d725
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-15 10:28:19 +0300

    10.15. Зачем нужно приведение типов

--- src/main/java/ru/cast/Main.java
+++ src/main/java/ru/cast/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Animal dog = new Dog();
         Animal guineaPig = new GuineaPig();
+        dog.sound();
     }
 }
---
+++ src/main/java/ru/cast/PolyUsage.java
@@ -0,0 +1,13 @@
+package ru.cast;
+
+public class PolyUsage {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Animal domesticAnimal = new DomesticAnimal();
+        Animal cow = new Cow();
+        Animal goose = new Goose();
+        Animal pet = new Pet();
+        Animal dog = new Dog();
+        Animal guineaPig = new GuineaPig();
+    }
+}
