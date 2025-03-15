dda4af49c5264cd9b8e165daf8859da8f64f6cbc
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-15 10:18:28 +0300

    10.15. Зачем нужно приведение типов

--- src/main/java/ru/cast/Cow.java
+++ src/main/java/ru/cast/Cow.java
@@ -1,6 +1,6 @@
 package ru.cast;
 
-public class Cow extends Animal {
+public class Cow extends DomesticAnimal {
     public void giveMilk() {
         System.out.println(nameClass + " дает человеку молоко.");
     }
