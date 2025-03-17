44e9f0ead0ad9af7996a470a7c123c089b8e80da
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-17 15:52:57 +0300

    10.21. Тестирование. Подготовка данных

--- src/test/java/ru/tracker/StartUITest.java
+++ src/test/java/ru/tracker/StartUITest.java
@@ -20,4 +20,15 @@ class StartUITest {
         expected = new Item("Fix Bug");
         assertThat(created.getName()).isEqualTo(expected.getName());
     }
+
+    @Test
+    void replaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item.");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId()), "edited item"};
+        StartUI.replaceItem(new MockInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("edited item");
+    }
 }
