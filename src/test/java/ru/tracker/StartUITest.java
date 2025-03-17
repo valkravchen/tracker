610384e927c4df5c6e04ab81aeb824fff05de485
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-17 19:51:43 +0300

    10.21. Тестирование. Подготовка данных

--- src/test/java/ru/tracker/StartUITest.java
+++ src/test/java/ru/tracker/StartUITest.java
@@ -24,11 +24,22 @@ class StartUITest {
     @Test
     void replaceItem() {
         Tracker tracker = new Tracker();
-        Item item = new Item("new item.");
+        Item item = new Item("new item");
         tracker.add(item);
         String[] answers = {String.valueOf(item.getId()), "edited item"};
         StartUI.replaceItem(new MockInput(answers), tracker);
         Item edited = tracker.findById(item.getId());
         assertThat(edited.getName()).isEqualTo("edited item");
+    }
+
+    @Test
+    void deleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId())};
+        StartUI.deleteItem(new MockInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertThat(deleted).isNull();
     }
 }
