5f809629240859d61fb5e35a9e9d321351cff786
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-17 09:08:03 +0300

    10.20. Программируемые объекты для тестирования

--- src/test/java/ru/tracker/StartUITest.java
+++ src/test/java/ru/tracker/StartUITest.java
@@ -1,17 +1,23 @@
 package ru.tracker;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 class StartUITest {
 
     @Test
     void whenAddItem() {
-      Input input = new ConsoleInput();
-      Tracker tracker = new Tracker();
-      StartUI.createItem(input, tracker);
-      Item created = tracker.findAll()[0];
-      Item expected = new Item("Fix PC");
-      assertThat(created.getName()).isEqualTo(expected.getName());
+        String[] answers = {"Fix PC", "Fix Bug"};
+        Input input = new MockInput(answers);
+        Tracker tracker = new Tracker();
+        StartUI.createItem(input, tracker);
+        StartUI.createItem(input, tracker);
+        Item created = tracker.findAll()[0];
+        Item expected = new Item("Fix PC");
+        assertThat(created.getName()).isEqualTo(expected.getName());
+        created = tracker.findAll()[1];
+        expected = new Item("Fix Bug");
+        assertThat(created.getName()).isEqualTo(expected.getName());
     }
 }
