8eac56c73518aabf4ff12048a0a548298b9ff756
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-19 16:15:03 +0300

    10.24. Тесты на StartUI

--- src/test/java/ru/tracker/StartUITest.java
+++ src/test/java/ru/tracker/StartUITest.java
@@ -20,27 +20,36 @@ class StartUITest {
 
     @Test
     void whenReplaceItem() {
-        Input input = new MockInput(new String[]{"0", "Item name", "1", "1", "Replace the name", "2"});
         Tracker tracker = new Tracker();
+        Item item1 = tracker.add(new Item("Первая заявка"));
+        Item item2 = tracker.add(new Item("Вторая заявка"));
+        Item item3 = tracker.add(new Item("Третья заявка"));
+        Input input = new MockInput(new String[]{"0", String.valueOf(item1.getId()), "Замена первой заявки", "1"
+        });
         UserAction[] actions = {
-                new CreateAction(),
                 new ReplaceAction(),
                 new ExitAction()
         };
         new StartUI().init(input, tracker, actions);
-        assertThat(tracker.findAll()[0].getName()).isEqualTo("Replace the name");
+        assertThat(tracker.findAll()[0].getName()).isEqualTo("Замена первой заявки");
     }
 
     @Test
     void whenDeleteItem() {
-        Input input = new MockInput(new String[]{"0", "Item name", "1", "1", "2"});
         Tracker tracker = new Tracker();
+        Item item1 = tracker.add(new Item("Первая заявка"));
+        Item item2 = tracker.add(new Item("Вторая заявка"));
+        Item item3 = tracker.add(new Item("Третья заявка"));
+        Input input = new MockInput(new String[]{"0", String.valueOf(item2.getId()), "1"});
         UserAction[] actions = {
-                new CreateAction(),
                 new DeleteAction(),
                 new ExitAction()
         };
         new StartUI().init(input, tracker, actions);
-        assertThat(tracker.findAll()).isEmpty();
+        Item deleted = tracker.findById(item2.getId());
+        assertThat(deleted).isNull();
     }
 }
+
+
+
