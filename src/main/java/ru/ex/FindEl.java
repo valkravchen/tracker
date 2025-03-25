407be0413a346172206c9ae1ad71b170a083c6e3
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-25 10:45:21 +0300

    11.5. Пользовательские исключения (практика)

---
+++ src/main/java/ru/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.ex;
+
+import java.util.Objects;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        for (int index = 0; index < value.length; index++) {
+            if (Objects.equals(key, value[index])) {
+                return index;
+            }
+         }
+        throw new ElementNotFoundException("Элемент '" + key + "' не найден в массиве");
+    }
+
+    public static void main(String[] args) {
+        String[] cities = {"Ростов-на-Дону", "Углич", "Шахты"};
+        String city = "Угл";
+        try {
+            int index = indexOf(cities, city);
+            System.out.println("Элемент найден на позиции: " + index);
+        } catch (ElementNotFoundException e) {
+           System.err.println("Ошибка: " + e.getMessage());
+        }
+    }
+}
