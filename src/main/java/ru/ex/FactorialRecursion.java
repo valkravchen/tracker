db48d1ccc97d154ef18caf32623e44418abaaa1f
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-26 06:39:38 +0300

    11.7. Error-исключения, связанные с работой JVM

---
+++ src/main/java/ru/ex/FactorialRecursion.java
@@ -0,0 +1,15 @@
+package ru.ex;
+
+public class FactorialRecursion {
+    public static int calc(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return calc(n - 1) * n;
+    }
+
+    public static void main(String[] args) {
+        int result = calc(2);
+        System.out.println(result);
+    }
+}
