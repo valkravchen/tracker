7615c8753c31b3f56418e36b065e54038028b5e1
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-28 05:47:05 +0300

    11.12. Замена if-else-throw на if-throw (теория)

--- src/main/java/ru/ex/Config.java
+++ src/main/java/ru/ex/Config.java
@@ -2,12 +2,12 @@ package ru.ex;
 
 public class Config {
     public static void load(String path) {
-        if (path != null) {
-            System.out.println("load config by " + path);
-        } else {
+        if (path == null) {
             throw new IllegalStateException("Path could not be null.");
         }
+        System.out.println("load config by " + path);
     }
+
 
     public static void main(String[] args) {
         load("jdbc://localhost:8080");
