d573c4db7ca47d82ee4f3e77a6392e829625e055
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-03-28 19:39:49 +0300

    11.13. Принципы раннего возврата и охранных выражений (теория)

--- src/main/java/ru/early/Example1.java
+++ src/main/java/ru/early/Example1.java
@@ -8,27 +8,19 @@ public class Example1 {
     private static final double COMPARING_PRECISION = 1.e-5;
 
     public static List<Double> calculateInverseProportionality(double k, double x1, double x2, int n) {
-        if (!isEquals(k, 0)) {
-            if (n > 0) {
-                if (k > 0 && x1 < x2) {
-                    return calculateFunction(k, x1, x2, n);
-                } else if (k < 0 && x2 < x1) {
-                    return calculateFunction(k, x2, x1, n);
-                } else if (isEquals(x1, x2)) {
-                    if (n == 1) {
-                        return List.of(k / x1);
-                    } else {
-                        throw new IllegalArgumentException("n suppose more than one point, but range include one point");
-                    }
-                } else {
-                    throw new IllegalArgumentException("k is not consistent with x1 and x2");
-                }
-            } else {
-                throw new IllegalArgumentException("'n' must be more than 0");
-            }
-        } else {
+        if (isEquals(k, 0)) {
             throw new IllegalArgumentException("'k' must be more than 0");
         }
+        if (n <= 0) {
+            throw new IllegalArgumentException("'n' must be more than 0");
+        }
+        if (k > 0 && x1 > x2 || k < 0 && x2 < x1) {
+            throw new IllegalArgumentException("k is not consistent with x1 and x2");
+        }
+        if (isEquals(x1, x2) && n > 1) {
+            throw new IllegalArgumentException("n suppose more than one point, but range include one point");
+        }
+        return calculateFunction(k, Math.min(x1, x2), Math.max(x1, x2), n);
     }
 
     private static boolean isEquals(double a, double b) {
