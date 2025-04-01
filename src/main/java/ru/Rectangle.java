c4d5089b235853fd92bfa7d28530521b5f2c5544
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-04-01 19:00:30 +0300

    12.3. Фабричный метод

---
+++ src/main/java/ru/Rectangle.java
@@ -0,0 +1,23 @@
+package ru;
+
+public class Rectangle {
+    int a;
+    int b;
+
+    public Rectangle(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public String draw() {
+        String ln = System.lineSeparator();
+        return "******" + ln
+                + "*    *" + ln
+                + "*    *" + ln
+                + "******";
+    }
+
+    public double square() {
+        return a * b;
+    }
+}
