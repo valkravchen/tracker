a41f665b1dffa0c3d562fa0496a4a2931e7e448c
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-04-05 19:57:08 +0300

    1

--- src/main/java/ru/ex/BackArray.java
+++ src/main/java/ru/ex/BackArray.java
@@ -3,6 +3,7 @@ package ru.ex;
 public class BackArray {
 
     public String[] turn(String[] names) {
+
         int middle = names.length / 2;
         for (int index = 0; index < middle; index++) {
             String temp = names[index];
