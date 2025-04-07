1c841dfd045459a237a3c71c827faf21ef039581
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-04-07 18:55:32 +0300

    12.4. final

---
+++ src/main/java/ru/ooa/Airbus.java
@@ -0,0 +1,36 @@
+package ru.ooa;
+
+public final class Airbus extends Aircraf {
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void printModel() {
+        System.out.println("Модель самолёта: " + name);
+    }
+
+    public void printCountEngine() {
+        int COUNT_ENGINE = (name.equals("A380")) ? 4 : 2;
+        System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
+
---
+++ src/main/java/ru/ooa/Aircraf.java
@@ -0,0 +1,7 @@
+package ru.ooa;
+
+public class Aircraf {
+    public void printModel() {
+        System.out.println("Метод выводит в консоль модель самолёта.");
+    }
+}
