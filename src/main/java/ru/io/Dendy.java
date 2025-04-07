adce031e7ddbc24c953e1a8b23f5f06546282c04
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-04-07 18:00:10 +0300

    1

--- src/main/java/ru/io/Dendy.java
+++ src/main/java/ru/io/Dendy.java
@@ -3,6 +3,7 @@ package ru.io;
 import java.util.Scanner;
 
 public class Dendy {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean run = true;
