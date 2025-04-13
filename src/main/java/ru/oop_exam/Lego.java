ddc1bc8b0db7bf81f2ee4fc2b6e94ddd28e16856
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-04-13 08:34:08 +0300

    Подготовка к экзамену по ООП

--- src/main/java/ru/oop_exam/Lego.java
+++ src/main/java/ru/oop_exam/Lego.java
@@ -1,5 +1,17 @@
 package ru.oop_exam;
 
 public class Lego {
+    private String shape;
 
+    public Lego(String shape) {
+        this.shape = shape;
+    }
+
+    void connect() {
+        System.out.println(shape + " соединяется");
+    }
+
+    void detach() {
+        System.out.println(shape + " отсоединяется");
+    }
 }
