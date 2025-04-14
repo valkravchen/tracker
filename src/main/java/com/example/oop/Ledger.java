a72ea16d7e700167c3aa4268bc799e74c257f3f5
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-04-14 19:08:48 +0300

    Подготовка к экзамену по ООП

---
+++ src/main/java/com/example/oop/Ledger.java
@@ -0,0 +1,23 @@
+package com.example.oop;
+
+public class Ledger {
+    private String owner;
+    private int amount;
+
+    public Ledger(String owner, int amount) {
+        this.owner = owner;
+        this.amount = amount;
+    }
+
+    public void check() {
+        System.out.println(owner + " имеет " + amount + " рублей");
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
