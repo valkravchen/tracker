dbf488316c714e96025c50b1e10dd6b92219257c
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-04-19 18:53:12 +0300

    Подготовка к экзамену по ООП

---
+++ src/main/java/com/example/oop/LedgerDemo.java
@@ -0,0 +1,8 @@
+package com.example.oop;
+
+public class LedgerDemo {
+    public static void main(String[] args) {
+        Ledger myLedger = new Ledger("Пример", 333);
+        myLedger.check();
+    }
+}
