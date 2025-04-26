c288a805787ab480a76d831389b32616e92c4ea9
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-04-26 08:08:10 +0300

    Подготовка к экзамену по ООП

---
+++ src/main/java/com/example/oop/CatDemo.java
@@ -0,0 +1,10 @@
+package com.example.oop;
+
+public class CatDemo {
+    public static void main(String[] args) {
+        Cat barsik = new Cat("Барсик");
+        Cat murzik = new Cat("Мурзик");
+        barsik.meow();
+        murzik.meow();
+    }
+}
