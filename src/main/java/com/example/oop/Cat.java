9fccd4c25aedef6ea82e7799bff61199524231a5
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-04-26 08:05:15 +0300

    Подготовка к экзамену по ООП

---
+++ src/main/java/com/example/oop/Cat.java
@@ -0,0 +1,13 @@
+package com.example.oop;
+
+public class Cat {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public void meow() {
+        System.out.println(name + " мяукает");
+    }
+}
