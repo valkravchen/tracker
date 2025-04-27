493ea9ee410da4f43022ce9593a0a60db8bea85e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-04-27 18:53:23 +0300

    Подготовка к экзамену по ООП

--- src/main/java/com/example/oop/Car.java
+++ src/main/java/com/example/oop/Car.java
@@ -3,4 +3,9 @@ package com.example.oop;
 public class Car {
     private String model;
     private String color;
+
+    public Car(String model, String color) {
+        this.model = model;
+        this.color = color;
+    }
 }
