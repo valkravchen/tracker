85cac37c98b3a33173f1a28ab42db7fc60716476
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-04-28 19:00:57 +0300

    Подготовка к экзамену по ООП

--- src/main/java/com/example/oop/Person.java
+++ src/main/java/com/example/oop/Person.java
@@ -3,4 +3,9 @@ package com.example.oop;
 public class Person {
     private String name;
     private int age;
+
+    public static void main(String[] args) {
+        Person p = new Person();
+        System.out.println(p.name + " " + p.age);
+    }
 }
