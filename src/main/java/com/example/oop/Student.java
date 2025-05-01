7c478f909305be63f2e62cb4519f4609b8412cd6
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-01 16:47:05 +0300

    Подготовка к экзамену по ООП

---
+++ src/main/java/com/example/oop/Student.java
@@ -0,0 +1,11 @@
+package com.example.oop;
+
+public class Student {
+    private String name;
+    private boolean isStudying;
+
+    public Student(String name) {
+        this.name = name;
+        this.isStudying = false;
+    }
+}
