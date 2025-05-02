c3c7f32a8c5b4b03bc00e19277ab17f9a806b4c4
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-02 19:19:51 +0300

    Подготовка к экзамену по ООП

--- src/main/java/com/example/oop/Car.java
+++ src/main/java/com/example/oop/Car.java
@@ -4,4 +4,10 @@ public class Car {
     private String model;
     private int speed;
     private boolean isRunning;
+
+    public Car(String model) {
+        this.model = model;
+        this.speed = 0;
+        this.isRunning = false;
+    }
 }
