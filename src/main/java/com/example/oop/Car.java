8ed5356e506d74f4d8e4f5a54d8e9582ae0958eb
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-04 19:51:02 +0300

    Подготовка к экзамену по ООП

--- src/main/java/com/example/oop/Car.java
+++ src/main/java/com/example/oop/Car.java
@@ -3,31 +3,14 @@ package com.example.oop;
 public class Car {
     private String model;
     private int speed;
-    private boolean isRunning;
 
     public Car(String model) {
         this.model = model;
         this.speed = 0;
-        this.isRunning = false;
     }
 
-    public void start() {
-        isRunning = true;
-        System.out.println(model + " запущен");
-    }
-
-    public void setSpeed(int speed) {
-        if (isRunning) {
-            this.speed = speed;
-            System.out.println(model + " движется со скоростью " + speed + " км/ч");
-        } else {
-            System.out.println(model + " не запущен, скорость не установлена");
-        }
-    }
-
-    public void showStatus() {
-        System.out.println(model + " имеет статус: " + (isRunning ? "Запущен" : "Остановлен")
-                + ", скорость: " + speed + " км/ч");
+    public int getSpeed() {
+        return speed;
     }
 
     public String getModel() {
