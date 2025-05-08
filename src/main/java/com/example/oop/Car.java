ffe8a1d4561ae350cf31e922cbac636e38c3f2f1
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-08 09:47:49 +0300

    Подготовка к экзамену по ООП

--- src/main/java/com/example/oop/Car.java
+++ src/main/java/com/example/oop/Car.java
@@ -3,4 +3,12 @@ package com.example.oop;
 public class Car {
     private String model;
     private int speed;
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
 }
