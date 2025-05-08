aaa43a3f11476c82d853574d88ee7691f4b2f61e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-08 10:12:56 +0300

    Подготовка к экзамену по ООП

--- src/main/java/com/example/oop/SportsCar.java
+++ src/main/java/com/example/oop/SportsCar.java
@@ -3,5 +3,10 @@ package com.example.oop;
 public class SportsCar extends Car {
     public SportsCar(String model) {
         super(model);
+
+    }
+    @Override
+    public void accelerate(int increment) {
+        super.accelerate(increment * 2);
     }
 }
