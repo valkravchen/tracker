17464f3dcc44acbd3432158994fc0da476dbe9cb
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-09 15:32:42 +0300

    Подготовка к экзамену по ООП

---
+++ src/main/java/com/example/oop/ConstructorDemo.java
@@ -0,0 +1,8 @@
+package com.example.oop;
+
+public class ConstructorDemo {
+    public static void main(String[] args) {
+        Constructor constructor = new Constructor();
+        System.out.println("name = " + constructor.getName() + "; number = " + constructor.getNumber());
+    }
+}
