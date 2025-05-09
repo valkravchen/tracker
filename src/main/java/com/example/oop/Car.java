481853a0b71c41edf4a03ab309f0be27e8cc3f7a
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-09 09:02:03 +0300

    Подготовка к экзамену по ООП

--- src/main/java/com/example/oop/Car.java
+++ src/main/java/com/example/oop/Car.java
@@ -3,14 +3,34 @@ package com.example.oop;
 public class Car implements Vehicle {
     private String model;
     private int speed;
+    private static int carCount;
 
-    public String getModel() {
-        return model;
+    static {
+        carCount = 0;
+        System.out.println("Класс Car загружен");
+    }
+
+    {
+        System.out.println("Создаётся новый объект Car");
+    }
+
+    public class Engine {
+        private int horsepower;
+
+        public Engine(int horsepower) {
+            this.horsepower = horsepower;
+            System.out.println("Двигатель для " + model + " создан");
+        }
+
+        public int getPower() {
+            return horsepower;
+        }
     }
 
     public Car(String model) {
         this.model = model;
         this.speed = 0;
+        carCount++;
     }
 
     @Override
@@ -18,13 +38,17 @@ public class Car implements Vehicle {
         System.out.println("Двигатель автомобиля " + model + " запущен");
     }
 
-    public int getSpeed() {
-        return speed;
-    }
-
     public void accelerate(int increment) {
         if (increment > 0) {
             speed += increment;
         }
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public static int getCarCount() {
+        return carCount;
     }
 }
