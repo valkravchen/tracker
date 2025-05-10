6eb9270c75021beb480615fa412f170368e3fc67
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-10 09:33:42 +0300

    Подготовка к экзамену по ООП

--- src/main/java/com/example/oop/Vehicle.java
+++ src/main/java/com/example/oop/Vehicle.java
@@ -1,4 +1,19 @@
 package com.example.oop;
 
 public class Vehicle {
+    private String brand;
+    private int speed;
+    private int id;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
