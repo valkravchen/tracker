2799102001f8dceeeb5028d5932de1d31620f6ae
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-10 16:49:47 +0300

    Подготовка к экзамену по ООП

--- src/main/java/com/example/oop/Car.java
+++ src/main/java/com/example/oop/Car.java
@@ -1,8 +1,16 @@
 package com.example.oop;
 
 public class Car implements Vehicle {
+    private String model;
+    private int speed;
+
+    public Car(String model) {
+        this.model = model;
+        this.speed = 0;
+    }
+
     @Override
     public void startEngine() {
-        System.out.println("Двигатель автомобиля "  + " запущен");
+        System.out.println("Двигатель автомобиля " + model + " запущен");
     }
 }
