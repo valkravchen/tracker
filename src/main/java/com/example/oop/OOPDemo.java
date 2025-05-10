12bbc70506946728e8c263a8f1d84c51a3446c73
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-10 17:12:40 +0300

    Подготовка к экзамену по ООП

--- src/main/java/com/example/oop/OOPDemo.java
+++ src/main/java/com/example/oop/OOPDemo.java
@@ -6,5 +6,10 @@ public class OOPDemo {
         car.startEngine();
         car.accelerate(30);
         System.out.println("Модель: " + car.getModel() + ", Скорость: " + car.getSpeed());
+
+        SportsCar sportsCar = new SportsCar("Ferrari");
+        sportsCar.startEngine();
+        sportsCar.turboBoost();
+        System.out.println("Модель: " + sportsCar.getModel() + ", Скорость: " + sportsCar.getSpeed());
     }
 }
