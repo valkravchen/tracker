30a011501e53b135675957618f2fc33627fb866e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-10 09:46:50 +0300

    Подготовка к экзамену по ООП

--- src/main/java/com/example/oop/Vehicle.java
+++ src/main/java/com/example/oop/Vehicle.java
@@ -5,6 +5,13 @@ public class Vehicle {
     private int speed;
     private int id;
 
+
+    public Vehicle(int id, String brand, int speed) {
+        this.id = id;
+        this.brand = brand;
+        this.speed = speed;
+    }
+
     public String getBrand() {
         return brand;
     }
@@ -15,5 +22,19 @@ public class Vehicle {
 
     public int getId() {
         return id;
+    }
+
+    public void move() {
+        System.out.println(brand + " движется со скоростью "  + speed);
+    }
+
+    public void stop() {
+        System.out.println("Остановка движения у " + brand);
+    }
+
+    public static void main(String[] args) {
+        Vehicle toyota = new Vehicle(1, "Toyota", 100);
+        toyota.move();
+        toyota.stop();
     }
 }
