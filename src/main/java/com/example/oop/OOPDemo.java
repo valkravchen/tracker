b7408f314290031e3e27311b8e4de3b233bff668
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-22 17:41:14 +0300

    Подготовка к экзамену по ООП

--- src/main/java/com/example/oop/OOPDemo.java
+++ src/main/java/com/example/oop/OOPDemo.java
@@ -3,6 +3,5 @@ package com.example.oop;
 public class OOPDemo {
     public static void main(String[] args) {
         Vehicle car1 = new Car("Жигули");
-        Vehicle car2 = new Car("Волга");
     }
 }
