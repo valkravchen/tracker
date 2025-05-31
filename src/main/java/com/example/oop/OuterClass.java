8a1c2a22b7ca00bdd83ee73fb153a5a8dcc69159
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-31 10:36:55 +0300

    Статические вложенные классы

---
+++ src/main/java/com/example/oop/OuterClass.java
@@ -0,0 +1,17 @@
+package com.example.oop;
+
+public class OuterClass {
+    private static String staticField = "Статическое поле";
+    private String instanceField = "поле экземпляра";
+
+    public static class Nested {
+        public void print() {
+            System.out.println(staticField);
+        }
+    }
+
+    public static void main(String[] args) {
+        Nested nested = new Nested();
+        nested.print();
+    }
+}
