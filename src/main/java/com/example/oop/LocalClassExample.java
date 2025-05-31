3096192f259d2f2bb3e04148841c12ecc194f484
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-31 12:41:37 +0300

    Локальные классы

---
+++ src/main/java/com/example/oop/LocalClassExample.java
@@ -0,0 +1,13 @@
+package com.example.oop;
+
+public class LocalClassExample {
+    public void printMessage() {
+        final String localVar = "Локальная переменная";
+        class LocalPrinter {
+            public void print() {
+                System.out.println(localVar);
+            }
+        }
+        new LocalPrinter().print();
+    }
+}
