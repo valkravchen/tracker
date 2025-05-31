5e6146e21c38389112bc7d61456c167255a5066a
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-31 05:29:43 +0300

    Подготовка к экзамену по ООП

--- src/main/java/com/example/oop/Car.java
+++ src/main/java/com/example/oop/Car.java
@@ -1,6 +1,17 @@
 package com.example.oop;
 
+import java.util.concurrent.CancellationException;
+
 public class Car {
     private String model;
     private int year;
+
+    public Car(String model, int year) {
+        this.model = model;
+        this.year = year;
+    }
+
+    public void displayInfo() {
+        System.out.println("Model: " + model + ", year " + year);
+    }
 }
