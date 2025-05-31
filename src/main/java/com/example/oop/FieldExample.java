3670747351f1165ed9efafbc3247bd6680d61048
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-31 06:15:46 +0300

    Подготовка к экзамену по ООП

---
+++ src/main/java/com/example/oop/FieldExample.java
@@ -0,0 +1,10 @@
+package com.example.oop;
+
+public class FieldExample {
+    public static String CLASS_NAME = "FieldExample";
+    private int instanceValue;
+
+    public FieldExample(int instanceValue) {
+        this.instanceValue = instanceValue;
+    }
+}
