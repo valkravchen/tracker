28b095005691e377cfb0944b2764fea12c1eadc5
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-31 14:39:38 +0300

    Статический блок

---
+++ src/main/java/com/example/oop/InstanceBlock.java
@@ -0,0 +1,17 @@
+package com.example.oop;
+
+public class InstanceBlock {
+    private String name;
+    {
+        name = "По умолчанию";
+        System.out.println("Блок экземляра выполнен");
+    }
+    public void InstanceBlockDemo() {
+        System.out.println("Конструктор выполнен");
+    }
+
+    public static void main(String[] args) {
+        InstanceBlock instanceBlock = new InstanceBlock();
+        instanceBlock.InstanceBlockDemo();
+    }
+}
