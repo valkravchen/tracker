d3dfa25001fb31ab70303a624d7c8eea49688952
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-31 12:49:16 +0300

    Локальные классы

--- src/main/java/com/example/oop/LocalClassExample.java
+++ src/main/java/com/example/oop/LocalClassExample.java
@@ -8,9 +8,5 @@ public class LocalClassExample {
                 System.out.println(localVar);
             }
         }
-        new LocalPrinter().print();
-    }
-
-    public static void main(String[] args) {
     }
 }
