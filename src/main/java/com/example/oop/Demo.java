2b19a9bac248c1b3444f4b7af3765290276a5d4c
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-31 09:25:22 +0300

    Статические методы

--- src/main/java/com/example/oop/Demo.java
+++ src/main/java/com/example/oop/Demo.java
@@ -2,9 +2,9 @@ package com.example.oop;
 
 public class Demo {
     public static void main(String[] args) {
-        BankAccount account = new BankAccount();
-        System.out.println(account.getBalance());
-        account.deposit(7);
-        System.out.println(account.getBalance());
+        double area = MathUtils.circleArea(3);
+        double max = MathUtils.max(3, 33);
+        System.out.println(area);
+        System.out.println(max);
     }
 }
---
+++ src/main/java/com/example/oop/MathUtils.java
@@ -0,0 +1,11 @@
+package com.example.oop;
+
+public class MathUtils {
+    public static double circleArea(double radius) {
+        return Math.PI * radius * radius;
+    }
+
+    public static int max(int a, int b) {
+        return a > b ? a : b;
+    }
+}
