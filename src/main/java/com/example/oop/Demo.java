948652c2aca09b9108178e87120c29b25c9aff1e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-31 06:20:23 +0300

    Подготовка к экзамену по ООП

--- src/main/java/com/example/oop/Demo.java
+++ src/main/java/com/example/oop/Demo.java
@@ -3,6 +3,8 @@ package com.example.oop;
 public class Demo {
     public static void main(String[] args) {
         Car car = new Car("Волга", 1989);
+        FieldExample fieldExample = new FieldExample(3);
         car.displayInfo();
+        fieldExample.displayInfo();
     }
 }
--- src/main/java/com/example/oop/FieldExample.java
+++ src/main/java/com/example/oop/FieldExample.java
@@ -7,4 +7,8 @@ public class FieldExample {
     public FieldExample(int instanceValue) {
         this.instanceValue = instanceValue;
     }
+
+    public void displayInfo() {
+        System.out.println("instanceValue: " + instanceValue + ", CLASS_NAME: " + CLASS_NAME);
+    }
 }
