0b5e090ff584f10b639c2efcc712c78ebee382b5
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-31 13:03:09 +0300

    Анонимные классы

--- src/main/java/com/example/oop/Demo.java
+++ src/main/java/com/example/oop/Demo.java
@@ -2,9 +2,12 @@ package com.example.oop;
 
 public class Demo {
     public static void main(String[] args) {
-        Book book = new Book();
-        Book book1 = new Book("Наука побеждать", "Александр Васильевич Суворов");
-        System.out.println("Название книги: " + book.getTittle() + "; автор книги: " + book.getAuthor());
-        System.out.println("Название книги: " + book1.getTittle() + "; автор книги: " + book1.getAuthor());
+        Greeting greeting = new Greeting() {
+            @Override
+            public void greet() {
+                System.out.println("Hello");
+            }
+        };
+        greeting.greet();
     }
 }
