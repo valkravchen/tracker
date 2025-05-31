0adcba9937a73f01e854a56603dbb8757848504f
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-05-31 07:52:53 +0300

    Подготовка к экзамену по ООП

---
+++ src/main/java/com/example/oop/Counter.java
@@ -0,0 +1,13 @@
+package com.example.oop;
+
+public class Counter {
+    public static int totalCount = 0;
+
+    public Counter() {
+        totalCount ++;
+    }
+
+    public void displayInfo() {
+        System.out.println("Количество: " + totalCount);
+    }
+}
--- src/main/java/com/example/oop/Demo.java
+++ src/main/java/com/example/oop/Demo.java
@@ -5,8 +5,11 @@ public class Demo {
         Car car = new Car("Волга", 1989);
         FieldExample fieldExample = new FieldExample(3);
         Person person = new Person("Александр", 33);
+        Counter counter1 = new Counter();
+        Counter counter2 = new Counter();
         car.displayInfo();
         fieldExample.displayInfo();
         person.displayInfo();
+        counter2.displayInfo();
     }
 }
