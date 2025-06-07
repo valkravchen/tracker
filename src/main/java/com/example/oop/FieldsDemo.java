0b46b15a81f9a80289a925dfd443ec2697573bf5
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-07 09:18:01 +0300

    3. Элементы класса. Поля (статические и нестатические). Ошибка доступа к нестатическому полю

--- src/main/java/com/example/oop/FieldsDemo.java
+++ src/main/java/com/example/oop/FieldsDemo.java
@@ -2,9 +2,6 @@ package com.example.oop;
 
 public class FieldsDemo {
     public static void main(String[] args) {
-        User user1 = new User("Иван", 19);
-        System.out.println(user1.name);
-        user1.name = "Ярослав";
-        System.out.println(user1.name);
+        System.out.println(User.name);
     }
 }
