c2c0421df62d0e21ef8abffbeff068135e00a8b0
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-07 09:07:53 +0300

    3. Элементы класса. Поля (статические и нестатические). Изменение статического поля

--- src/main/java/com/example/oop/FieldsDemo.java
+++ src/main/java/com/example/oop/FieldsDemo.java
@@ -4,6 +4,7 @@ public class FieldsDemo {
     public static void main(String[] args) {
         System.out.println("idCounter: " + User.getIdCounter());
         User user1 = new User("Иван", 19);
+        User.setIdCounter(100);
         System.out.println("idCounter: " + User.getIdCounter());
         User user2 = new User("Александр", 25);
         System.out.println("idCounter: " + User.getIdCounter());
