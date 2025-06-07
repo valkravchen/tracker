e57ee840b55168eb06483adadbafde00a4c65e65
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-07 08:46:19 +0300

    3. Элементы класса. Поля (статические и нестатические). Уникальность нестатических полей

--- src/main/java/com/example/oop/FieldsDemo.java
+++ src/main/java/com/example/oop/FieldsDemo.java
@@ -3,7 +3,7 @@ package com.example.oop;
 public class FieldsDemo {
     public static void main(String[] args) {
         User user1 = new User("Иван", 19);
-        User user2 = new User("Александр",25);
+        User user2 = new User("Александр", 25);
         System.out.println("Имя пользователя 1: " + user1.getName()
                 + "; возраст пользователя 1: " + user1.getAge());
         System.out.println("Имя пользователя 2: " + user2.getName()
