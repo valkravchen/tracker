9ca8cd25f88b3c2ee17d55053fc11d37af16bb37
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-07 08:33:45 +0300

    3. Элементы класса. Поля (статические и нестатические)

--- src/main/java/com/example/oop/User.java
+++ src/main/java/com/example/oop/User.java
@@ -4,5 +4,31 @@ public class User {
     private static int idCounter = 0;
     private String name;
     private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+        idCounter++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static int getIdCounter() {
+        return idCounter;
+    }
+
+    public static void setIdCounter(int value) {
+        idCounter = value;
+    }
 }
 
