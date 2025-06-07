00b097404906470094cd576c829f50a3c13e216c
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-07 18:37:08 +0300

    3. Элементы класса. Конструкторы Влияние конструктора на статическое поле

--- src/main/java/com/example/oop/ConstructorsDemo.java
+++ src/main/java/com/example/oop/ConstructorsDemo.java
@@ -2,8 +2,10 @@ package com.example.oop;
 
 public class ConstructorsDemo {
     public static void main(String[] args) {
-        User user1 = new User("Добрыня");
-        System.out.println("Имя пользователя: " + user1.getName() + "; возраст: " + user1.getAge());
+        System.out.println("IdCounter: " + User.getIdCounter());
+        User user1 = new User("Добрыня Никитич");
+        System.out.println("IdCounter: " + User.getIdCounter());
+        User user2 = new User("Илья Муромец");
         System.out.println("IdCounter: " + User.getIdCounter());
     }
 }
