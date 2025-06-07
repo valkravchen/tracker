c2faec7d2a3cc616c6d0b197ed8d40cd0199124f
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-07 13:56:44 +0300

    3. Элементы класса. Методы Вызов статического метода через объект

--- src/main/java/com/example/oop/MethodsDemo.java
+++ src/main/java/com/example/oop/MethodsDemo.java
@@ -2,10 +2,9 @@ package com.example.oop;
 
 public class MethodsDemo {
     public static void main(String[] args) {
-        System.out.println("IdCounter: " + User.getIdCounter());
         User user1 = new User("Лев", 36);
-        System.out.println("IdCounter: " + User.getIdCounter());
-        User user2 = new User("Фёдор", 56);
-        System.out.println("IdCounter: " + User.getIdCounter());
+        System.out.println(user1.getIdCounter());
+        user1.resetCounter();
+        System.out.println(user1.getIdCounter());
     }
 }
