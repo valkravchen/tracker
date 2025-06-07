3fab0816057cda527b71e8ee4594111bf155367d
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-07 09:15:31 +0300

    3. Элементы класса. Поля (статические и нестатические). Прямой доступ к полям

--- src/main/java/com/example/oop/FieldsDemo.java
+++ src/main/java/com/example/oop/FieldsDemo.java
@@ -2,11 +2,9 @@ package com.example.oop;
 
 public class FieldsDemo {
     public static void main(String[] args) {
-        System.out.println("idCounter: " + User.getIdCounter());
         User user1 = new User("Иван", 19);
-        User.setIdCounter(100);
-        System.out.println("idCounter: " + User.getIdCounter());
-        User user2 = new User("Александр", 25);
-        System.out.println("idCounter: " + User.getIdCounter());
+        System.out.println(user1.name);
+        user1.name = "Ярослав";
+        System.out.println(user1.name);
     }
 }
