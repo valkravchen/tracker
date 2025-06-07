c3d15547df99cabfd33fb023c3942f0621870eac
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-07 13:44:28 +0300

    3. Элементы класса. Методы Вызов нестатических методов

--- src/main/java/com/example/oop/MethodsDemo.java
+++ src/main/java/com/example/oop/MethodsDemo.java
@@ -1,4 +1,12 @@
 package com.example.oop;
 
 public class MethodsDemo {
+    public static void main(String[] args) {
+        User user1 = new User("Лев", 36);
+        User user2 = new User("Фёдор", 56);
+        System.out.println(user1.getDetails());
+        System.out.println(user2.getDetails());
+        user1.setAge(37);
+        System.out.println(user1.getDetails());
+    }
 }
