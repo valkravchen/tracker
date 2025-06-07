a9794154b69db70e32d4bc1f972ffc0452b81c13
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-07 09:02:32 +0300

    3. Элементы класса. Поля (статические и нестатические). Общность статического поля

--- src/main/java/com/example/oop/FieldsDemo.java
+++ src/main/java/com/example/oop/FieldsDemo.java
@@ -2,11 +2,10 @@ package com.example.oop;
 
 public class FieldsDemo {
     public static void main(String[] args) {
+        System.out.println("idCounter: " + User.getIdCounter());
         User user1 = new User("Иван", 19);
+        System.out.println("idCounter: " + User.getIdCounter());
         User user2 = new User("Александр", 25);
-        System.out.println("Имя пользователя 1: " + user1.getName()
-                + "; возраст пользователя 1: " + user1.getAge());
-        System.out.println("Имя пользователя 2: " + user2.getName()
-                + "; возраст пользователя 2: " + user2.getAge());
+        System.out.println("idCounter: " + User.getIdCounter());
     }
 }
