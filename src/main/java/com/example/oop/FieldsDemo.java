efbe965818ea696a91a75c110315593e3a32e556
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-07 13:36:49 +0300

    3. Элементы класса. Методы Вызов нестатических методов

--- src/main/java/com/example/oop/FieldsDemo.java
+++ src/main/java/com/example/oop/FieldsDemo.java
@@ -4,7 +4,6 @@ public class FieldsDemo {
     public static void main(String[] args) {
         User user1 = new User("Иван", 19);
         System.out.println(user1.getName());
-        user1.setName("Александр");
         System.out.println(user1.getName());
     }
 }
