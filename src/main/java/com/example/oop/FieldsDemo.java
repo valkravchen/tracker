02bc5190aba2a737ac8e9decfe4aaa40fad4eb6e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-07 09:30:26 +0300

    3. Элементы класса. Поля (статические и нестатические). Добавление final

--- src/main/java/com/example/oop/FieldsDemo.java
+++ src/main/java/com/example/oop/FieldsDemo.java
@@ -4,7 +4,7 @@ public class FieldsDemo {
     public static void main(String[] args) {
         User user1 = new User("Иван", 19);
         System.out.println(user1.getName());
-        user1 = user1.setName("Александр");
+        user1.setName("Александр");
         System.out.println(user1.getName());
     }
 }
--- src/main/java/com/example/oop/User.java
+++ src/main/java/com/example/oop/User.java
@@ -19,6 +19,10 @@ public class User {
         return age;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void setAge(int age) {
         this.age = age;
     }
