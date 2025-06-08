d6b4bf16b991bfe9a620aac6dc424545d1a34234
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-08 05:36:32 +0300

    3. Элементы класса. Вложенные классы Статический вложенный класс

--- src/main/java/com/example/oop/NestedClassesDemo.java
+++ src/main/java/com/example/oop/NestedClassesDemo.java
@@ -2,5 +2,9 @@ package com.example.oop;
 
 public class NestedClassesDemo {
     public static void main(String[] args) {
+        User.StaticNested nested = new User.StaticNested();
+        nested.display();
+        User user = new User("Семён Семёныч", 40);
+        nested.display();
     }
 }
