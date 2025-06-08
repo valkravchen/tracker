ccedea4dade79cbc66ae73320debed2706ead1bf
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-08 05:28:40 +0300

    3. Элементы класса. Вложенные классы Статический вложенный класс

--- src/main/java/com/example/oop/ConstructorsDemo.java
+++ src/main/java/com/example/oop/ConstructorsDemo.java
@@ -1,11 +1,4 @@
 package com.example.oop;
 
 public class ConstructorsDemo {
-    public static void main(String[] args) {
-        System.out.println("IdCounter: " + User.getIdCounter());
-        User user1 = new User("Добрыня Никитич");
-        System.out.println("IdCounter: " + User.getIdCounter());
-        User user2 = new User("Илья Муромец");
-        System.out.println("IdCounter: " + User.getIdCounter());
-    }
 }
