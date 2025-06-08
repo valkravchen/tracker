ecceeef5487772b92afb4941a5e6bbf8e68ccc66
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-08 05:54:06 +0300

    3. Элементы класса. Вложенные классы Внутренний класс

--- src/main/java/com/example/oop/NestedClassesDemo.java
+++ src/main/java/com/example/oop/NestedClassesDemo.java
@@ -3,7 +3,7 @@ package com.example.oop;
 public class NestedClassesDemo {
     public static void main(String[] args) {
         User user = new User("Семён Семёныч", 40);
-        User.Inner inner =user.new Inner();
+        User.Inner inner = user.new Inner();
         inner.display();
     }
 }
