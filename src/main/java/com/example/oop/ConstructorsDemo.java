63e642afa5d7e868ea7b4a9797e815f44368b731
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-10 18:59:34 +0300

    Что такое конструктор в Java?

--- src/main/java/com/example/oop/ConstructorsDemo.java
+++ src/main/java/com/example/oop/ConstructorsDemo.java
@@ -2,8 +2,10 @@ package com.example.oop;
 
 public class ConstructorsDemo {
     public static void main(String[] args) {
-        User user = new User("Кощей");
-        System.out.println(user.getName() + ", " + user.getAge());
+        User user1 = new User("Кощей");
+        User user2 = new User("Леший", 333);
+        System.out.println(user1.getName() + ", " + user1.getAge());
+        System.out.println(user2.getName() + ", " + user2.getAge());
         System.out.println("Счётчик: " + User.getIdCounter());
     }
 }
