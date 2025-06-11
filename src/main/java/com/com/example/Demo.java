7bb9ec83298da0d75d7f4e4ce36e333cdafeefea
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-11 20:00:04 +0300

    Автоматический вызов super()

--- src/main/java/com/com/example/Demo.java
+++ src/main/java/com/com/example/Demo.java
@@ -2,7 +2,9 @@ package com.com.example;
 
 public class Demo {
     public static void main(String[] args) {
-        Child child = new Child(33);
-        System.out.println("Имя: " + child.getName() + "; возраст: " + child.getAge());
+        Child child1 = new Child("Добрыня", 33);
+        System.out.println("Имя: " + child1.getName() + "; возраст: " + child1.getAge());
+        Child child2 = new Child();
+        System.out.println("Имя: " + child2.getName() + "; возраст: " + child2.getAge());
     }
 }
--- src/main/java/com/com/example/Parent.java
+++ src/main/java/com/com/example/Parent.java
@@ -5,7 +5,12 @@ public class Parent {
 
     public Parent() {
         this.name = "Безымянный";
-        System.out.println("Родительский класс" );
+        System.out.println("Родительский класс. Первый конструктор");
+    }
+
+    public Parent(String name) {
+        this.name = name;
+        System.out.println("Родительский класс. Второй конструктор");
     }
 
     public String getName() {
