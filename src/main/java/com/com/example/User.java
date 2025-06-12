b4dfde61de215c4e99cde2fbcfb5966405cfadbe
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-12 05:43:39 +0300

    Перегрузка конструктора

---
+++ src/main/java/com/com/example/User.java
@@ -0,0 +1,25 @@
+package com.com.example;
+
+public class User {
+    private static int idCounter = 0;
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+        idCounter++;
+    }
+
+    public static int getIdCounter() {
+        return idCounter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
