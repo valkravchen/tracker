b1eca48488e23fc6acd0f625781b5333df78e9da
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-12 14:39:07 +0300

    Статический метод. Вызов через объект (пример ловушки)

--- src/main/java/com/com/example/Demo.java
+++ src/main/java/com/com/example/Demo.java
@@ -2,10 +2,7 @@ package com.com.example;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println(User.getIdCounter());
         User user = new User("Бабай", 0);
-        System.out.println(User.getIdCounter());
-        User.resetCounter();
-        System.out.println(User.getIdCounter());
+        System.out.println(user.getIdCounter());
     }
 }
