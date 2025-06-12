d4268f676085ed0effea1ed9e88261878ab1f7f8
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-12 10:38:31 +0300

    Вызов this()

--- src/main/java/com/com/example/Demo.java
+++ src/main/java/com/com/example/Demo.java
@@ -3,7 +3,7 @@ package com.com.example;
 public class Demo {
     public static void main(String[] args) {
         User user1 = new User("Леопольд", 5);
-        User user2 = new User();
+        User user2 = new User("H");
         System.out.println("Имя: " + user1.getName() + "; возраст: " + user1.getAge());
         System.out.println("Имя: " + user2.getName() + "; возраст: " + user2.getAge());
         System.out.println("Счётчик: " + User.getIdCounter());
--- src/main/java/com/com/example/User.java
+++ src/main/java/com/com/example/User.java
@@ -6,14 +6,14 @@ public class User {
     private int age;
 
     public User(String name, int age) {
-       this.name = name;
+       this(name);
        this.age = age;
         idCounter++;
         System.out.println("Вызов первого конструктора");
     }
 
-    public User() {
-       this("Безымянный", 0);
+    public User(String name) {
+       this.name = "Нет имени";
     }
 
     public static int getIdCounter() {
