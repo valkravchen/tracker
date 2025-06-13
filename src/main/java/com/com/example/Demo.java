f3c65abd6409a49bd0208e63f3c66055cead3e23
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-13 13:50:15 +0300

    Нестатические методы. Проверка сеттера setAge с некорректным значением (ловушка)

--- src/main/java/com/com/example/Demo.java
+++ src/main/java/com/com/example/Demo.java
@@ -3,11 +3,8 @@ package com.com.example;
 public class Demo {
     public static void main(String[] args) {
         User user1 = new User("Шурик", 21);
-        User user2 = new User("Ералаш", 15);
-        User user3 = new User("Каламбур", 55);
-        user3.setActive(false);
-        System.out.println(user1.getUserStatus());
-        System.out.println(user2.getUserStatus());
-        System.out.println(user3.getUserStatus());
+        System.out.println(user1.getAge());
+        user1.setAge(-5);
+        System.out.println(user1.getAge());
     }
 }
