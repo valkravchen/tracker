d5f5451ab76b948f95ff99dac1d255ab5d2903fb
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-13 13:35:05 +0300

    Нестатические методы. Использование геттеров для чтения состояния

--- src/main/java/com/com/example/Demo.java
+++ src/main/java/com/com/example/Demo.java
@@ -2,11 +2,9 @@ package com.com.example;
 
 public class Demo {
     public static void main(String[] args) {
-        User user1 = new User("Первый пользователь", 22);
-        User user2 = new User("Второй пользователь", 18);
-        System.out.println(user1.getInfo());
-        System.out.println(user2.getInfo());
-        user1.setInfo("Изменено имя первого пользователя", 23);
-        System.out.println(user1.getInfo());
+        User user = new User("Шурик", 21);
+        System.out.println(user.getName());
+        System.out.println(user.getAge());
+        System.out.println(user.isActive());
     }
 }
