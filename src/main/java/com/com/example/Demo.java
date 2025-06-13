86cf1abd38615df5ceddcb5ec008a171fe218945
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-13 13:40:18 +0300

    Нестатические методы. Использование сеттеров для изменения состояния

--- src/main/java/com/com/example/Demo.java
+++ src/main/java/com/com/example/Demo.java
@@ -3,6 +3,9 @@ package com.com.example;
 public class Demo {
     public static void main(String[] args) {
         User user = new User("Шурик", 21);
+        user.setName("Шурик Ы");
+        user.setAge(22);
+        user.setActive(false);
         System.out.println(user.getName());
         System.out.println(user.getAge());
         System.out.println(user.isActive());
