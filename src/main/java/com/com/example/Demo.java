209c16d0c79c217ec1788bd32e93873e215edf41
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-13 14:25:29 +0300

    Нестатические методы. Проверка canAccessPremium с разными состояниями

--- src/main/java/com/com/example/Demo.java
+++ src/main/java/com/com/example/Demo.java
@@ -2,10 +2,12 @@ package com.com.example;
 
 public class Demo {
     public static void main(String[] args) {
-        User user = new User("Шурик", 21);
-        System.out.println(user.getProfile());
-        user.updateProfile("Шурик Ы", 22, false);
-        System.out.println(user.getProfile());
-        System.out.println(user.isActive());
+        User user1 = new User("Шурик", 21);
+        User user2 = new User("Бывалый", 22);
+        User user3 = new User("Балбес", 14);
+        user1.setActive(false);
+        System.out.println(user1.canAccessPremium());
+        System.out.println(user2.canAccessPremium());
+        System.out.println(user1.canAccessPremium());
     }
 }
