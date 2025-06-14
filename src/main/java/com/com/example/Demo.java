a5fe95d614bde0269a60378a5a6665c0cfc5c841
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-14 10:20:26 +0300

    Нестатические методы

--- src/main/java/com/com/example/Demo.java
+++ src/main/java/com/com/example/Demo.java
@@ -2,9 +2,8 @@ package com.com.example;
 
 public class Demo {
     public static void main(String[] args) {
-        User user1 = new User("Пионер", 9);
-        User user2 = new User("Буратино", 5);
-        System.out.println(user1.getDetailedStatus());
-        System.out.println(user2.getDetailedStatus());
+        User user = new User("Буратино", 5);
+        user.updateProfile("Мальвина", 3, false);
+        System.out.println(user.getProfile());
     }
 }
