cba43dd5bdb8af5fc97405fa15638e65a6a4f477
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-15 15:46:27 +0300

    Базовый родительский класс Object в Java

--- src/main/java/com/com/example/Demo.java
+++ src/main/java/com/com/example/Demo.java
@@ -2,5 +2,7 @@ package com.com.example;
 
 public class Demo {
     public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("Буратино");
+        System.out.println(simpleUser.getClass().getSuperclass());
     }
 }
---
+++ src/main/java/com/com/example/SimpleUser.java
@@ -0,0 +1,9 @@
+package com.com.example;
+
+public class SimpleUser extends Object {
+    String name;
+
+    SimpleUser(String name) {
+        this.name = name;
+    }
+}
