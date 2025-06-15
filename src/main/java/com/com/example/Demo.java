75a3dd8184024547e0aa0299c103de55d95de1c4
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-15 16:48:43 +0300

    Базовый родительский класс Object в Java. Проверка родительского класса

--- src/main/java/com/com/example/Demo.java
+++ src/main/java/com/com/example/Demo.java
@@ -2,7 +2,7 @@ package com.com.example;
 
 public class Demo {
     public static void main(String[] args) {
-        Object object = new User("Буратино", 10);
-        System.out.println(object.toString());
+        User user = new User("Буратино", 10);
+        System.out.println(user.getClass().getSuperclass());
     }
 }
