d14d3422932616c9414acfb83bd9d7aa25d244c2
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-15 16:51:51 +0300

    Базовый родительский класс Object в Java. Метод Object без переопределения

--- src/main/java/com/com/example/Demo.java
+++ src/main/java/com/com/example/Demo.java
@@ -3,6 +3,6 @@ package com.com.example;
 public class Demo {
     public static void main(String[] args) {
         User user = new User("Буратино", 10);
-        System.out.println(user.getClass().getSuperclass());
+        System.out.println(user.toString());
     }
 }
