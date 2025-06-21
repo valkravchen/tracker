e905e420f5e0f871099a2265426605ac8aed2a70
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-21 10:38:24 +0300

    Анонимный класс (anonymous class)

--- src/main/java/com/com/example/Outer.java
+++ src/main/java/com/com/example/Outer.java
@@ -3,11 +3,13 @@ package com.com.example;
 public class Outer {
     void printMessage() {
         final String localVar = "Здравстсвуй, локальный класс!";
+
         class Local {
             void print() {
                 System.out.println(localVar);
             }
         }
+
         Local local = new Local();
         local.print();
     }
