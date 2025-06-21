15839ece25330bc610514955f986623d70a3f278
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-21 10:18:17 +0300

    Внутренний класс (inner class)

--- src/main/java/com/com/example/Outer.java
+++ src/main/java/com/com/example/Outer.java
@@ -1,9 +1,9 @@
 package com.com.example;
 
 public class Outer {
-    private  String message = "Здравствуй, inner class!";
+    private String message = "Здравствуй, inner class!";
 
-    class Inner{
+    class Inner {
         void print() {
             System.out.println(message);
         }
