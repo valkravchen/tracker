199985d040f0d4e5cbf1fb036f2389d751639526
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-21 16:57:24 +0300

    Анонимный класс (Anonymous Class)

--- src/main/java/com/com/example/Main.java
+++ src/main/java/com/com/example/Main.java
@@ -2,5 +2,13 @@ package com.com.example;
 
 public class Main {
     public static void main(String[] args) {
+        Greeting greeting = new Greeting() {
+            @Override
+            public void greet() {
+                System.out.println("Привет из анонимного класса!");
+            }
+        };
+
+        greeting.greet();
     }
 }
