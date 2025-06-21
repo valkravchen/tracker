350c45d723c2bb4a2b5b5857dd1bf184da29d7a9
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-21 10:18:26 +0300

    Внутренний класс (inner class)

--- src/main/java/com/com/example/Main.java
+++ src/main/java/com/com/example/Main.java
@@ -2,7 +2,7 @@ package com.com.example;
 
 public class Main {
     public static void main(String[] args) {
-       Outer outer = new Outer();
+        Outer outer = new Outer();
         Outer.Inner inner = outer.new Inner();
         inner.print();
     }
