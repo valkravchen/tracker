8181213abd9b5f6cf04f00e06424c0998ac02c6a
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-21 15:51:53 +0300

    Статический вложенный класс (Static Nested Class) в Java

--- src/main/java/com/com/example/Main.java
+++ src/main/java/com/com/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 100000; i++) {
             Outer.Nested obj = new Outer.Nested();
-            System.out.println(obj);
+            obj.print();
         }
     }
 }
