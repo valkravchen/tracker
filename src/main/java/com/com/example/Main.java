c9ffb12dbabd15a0a5bf32a2fe5a04693386f6aa
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-21 16:35:16 +0300

    Локальный класс

--- src/main/java/com/com/example/Main.java
+++ src/main/java/com/com/example/Main.java
@@ -2,9 +2,5 @@ package com.com.example;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 100000; i++) {
-            Outer.Nested obj = new Outer.Nested();
-            obj.print();
-        }
     }
 }
--- src/main/java/com/com/example/Outer.java
+++ src/main/java/com/com/example/Outer.java
@@ -1,9 +1,23 @@
 package com.com.example;
 
 public class Outer {
-    static class Nested {
-        void print() {
-            System.out.println("Объект Nested");
+    void printMessage(String message) {
+        int repeatCount = 3;
+
+        class MessagePrinter {
+            void print() {
+                for (int i = 0; i < repeatCount; i++) {
+                    System.out.println(message);
+                }
+            }
         }
+
+        MessagePrinter messagePrinter = new MessagePrinter();
+        messagePrinter.print();
+    }
+
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        outer.printMessage("Привет от локального класса!");
     }
 }
