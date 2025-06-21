81f8b358fc3ea9638422f6a40f9728cf85d16867
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-21 17:29:58 +0300

    Анонимный класс (Anonymous Class)

--- src/main/java/com/com/example/Outer.java
+++ src/main/java/com/com/example/Outer.java
@@ -1,21 +1,15 @@
 package com.com.example;
 
 public class Outer {
-   void printMessage(String message) {
-       int repeat = 2;
-       Runnable r = new Runnable() {
-           @Override
-           public void run() {
-               for (int i = 0; i < repeat; i++) {
-                   System.out.println(message);
-               }
-           }
-       };
-       new Thread(r).start();
-   }
+   Runnable task = new Runnable() {
+       @Override
+       public void run() {
+           System.out.println("Проверка анонимности");
+       }
+   };
 
     public static void main(String[] args) {
-       Outer outer = new Outer();
-       outer.printMessage("Здравия!");
+        Outer outer = new Outer();
+        outer.task.run();
     }
 }
