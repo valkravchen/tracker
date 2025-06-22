a25142b1b1c60579df5068720e8efe9dfcfb129e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-22 17:18:31 +0300

    Enum (Перечисление) в Java

--- src/main/java/com/example/oop/Enum/Main.java
+++ src/main/java/com/example/oop/Enum/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Day today = Day.SUNDAY;
         System.out.println(today);
+        System.out.println(today.getRussianName());
     }
 }
