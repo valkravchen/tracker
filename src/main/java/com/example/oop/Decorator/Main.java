c6a9222a75edce00a77117ea6108de1feeba8a93
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-22 09:06:37 +0300

    Шаблон проектирования "Декоратор" (Decorator)

--- src/main/java/com/example/oop/Decorator/Main.java
+++ src/main/java/com/example/oop/Decorator/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Coffee coffee = new SimpleCoffee();
         System.out.println(coffee.getDescription() + " = " + coffee.getCoast() + " руб.");
+        coffee = new MilkDecorator(coffee);
+        System.out.println(coffee.getDescription() + " = " + coffee.getCoast() + " руб.");
+        coffee = new SugarDecorator(coffee);
+        System.out.println(coffee.getDescription() + " = " + coffee.getCoast() + " руб.");
     }
 }
