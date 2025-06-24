4ad401a6d9d5e113bfdfb64c3f7046af1787e491
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-24 19:25:46 +0300

    Подготовка

--- src/main/java/com/example/oop/Student.java
+++ src/main/java/com/example/oop/Student.java
@@ -1,4 +1,21 @@
 package com.example.oop;
 
 public class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Student(String name) {
+        this.name = name;
+        this.age = 18;
+    }
+
+    public Student() {
+        this.name = "Безымянный";
+        this.age = 18;
+    }
 }
