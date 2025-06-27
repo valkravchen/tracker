d67324da7991dbb732ef07121af103709f7159a3
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-27 19:23:08 +0300

    Что такое конструктор?

--- src/main/java/com/example/oop/constructor/Car.java
+++ src/main/java/com/example/oop/constructor/Car.java
@@ -4,4 +4,18 @@ public class Car {
     private String make;
     private String model;
     private int year;
+
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public Car(String make, String model) {
+        this(make, model, 2025);
+    }
+
+    public Car() {
+        this("Марка неизвестна", "Модель неизвестна", 2025);
+    }
 }
