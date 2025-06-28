19fc7e2f6d7475274147990bb848375bb908d976
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-28 17:59:43 +0300

    Элементы класса

--- src/main/java/com/example/oop/order/Order.java
+++ src/main/java/com/example/oop/order/Order.java
@@ -1,4 +1,8 @@
 package com.example.oop.order;
 
 public class Order {
+    private String orderId;
+    private String description;
+    private double total;
+    private static int nextOrderId;
 }
