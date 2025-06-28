5848ee7d131e107f16b3a68f28e29571f1580d36
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-28 18:33:55 +0300

    Элементы класса

--- src/main/java/com/example/oop/order/Order.java
+++ src/main/java/com/example/oop/order/Order.java
@@ -5,4 +5,33 @@ public class Order {
     private String description;
     private double total;
     private static int nextOrderId;
+
+    static {
+        nextOrderId = 100;
+    }
+
+    {
+        orderId = "ORD: " + nextOrderId++;
+    }
+
+    public Order(String description, double total) {
+        this.description = validateDescription(description) ? description : "Некорректное описание";
+        this.total = validateTotal(total) ? total : 0.0;
+    }
+
+    public Order(String description) {
+        this(description, 0.0);
+    }
+
+    public Order() {
+        this("Пусто", 0.0);
+    }
+
+    private boolean validateDescription(String description) {
+        return description != null && !description.trim().isEmpty();
+    }
+
+    private boolean validateTotal(double total) {
+        return total >= 0.0;
+    }
 }
