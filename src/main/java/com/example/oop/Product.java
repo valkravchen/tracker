ebd7aef1f93b00d840cc1c3f02d49f40f8b27298
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-29 19:34:18 +0300

    Конструктор

--- src/main/java/com/example/oop/Product.java
+++ src/main/java/com/example/oop/Product.java
@@ -6,9 +6,9 @@ public class Product {
     private double price;
 
     public Product(String productId, String name, double price) {
-        this.productId = productId;
-        this.name = name;
-        this.price = price;
+        this.productId = (productId != null && !productId.trim().isEmpty() ? productId : "P000");
+        this.name = validateName(name) ? name : "некорректное название";
+        this.price = validatePrice(price) ? price : 10.0;
     }
 
     public Product(String name, double price) {
@@ -17,5 +17,13 @@ public class Product {
 
     public Product() {
         this("P000", "неизвестно", 10.0);
+    }
+
+    private boolean validateName(String name) {
+        return name != null && !name.trim().isEmpty();
+    }
+
+    private boolean validatePrice(double price) {
+        return price >= 0;
     }
 }
