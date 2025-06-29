c753d888df1116ea98b1b300557f9d7ff4e31508
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-06-29 12:15:33 +0300

    Элементы класса

--- src/main/java/com/example/oop/LibraryBook.java
+++ src/main/java/com/example/oop/LibraryBook.java
@@ -5,4 +5,29 @@ public class LibraryBook {
     private String title;
     private double rentalPrice;
     private static int nextBookId;
+
+    static {
+        nextBookId = 1000;
+    }
+
+    {
+        bookId = "Книга № " + nextBookId++;
+    }
+
+    public LibraryBook(String title, double rentalPrice) {
+        this.title = validateTitle(title) ? title : "некорректное название";
+        this.rentalPrice = validateRentalPrice(rentalPrice) ? rentalPrice : 5.0;
+    }
+
+    public LibraryBook(String title) {
+        this(title, 5.0);
+    }
+
+    private boolean validateTitle(String title) {
+        return title != null && !title.trim().isEmpty();
+    }
+
+    private boolean validateRentalPrice(double price) {
+        return price >= 0.0;
+    }
 }
