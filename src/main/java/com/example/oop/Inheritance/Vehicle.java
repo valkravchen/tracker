ea945a9eb07d9ec7482e34f8709d8462ef3cbe78
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-01 19:46:13 +0300

    Наследование в Java

--- src/main/java/com/example/oop/Inheritance/Vehicle.java
+++ src/main/java/com/example/oop/Inheritance/Vehicle.java
@@ -1,4 +1,25 @@
 package com.example.oop.Inheritance;
 
 public class Vehicle {
+    private String brand;
+    private int year;
+
+    public Vehicle(String brand, int year) {
+        this.brand = validateBrand(brand);
+        this.year = validateYear(year);
+    }
+
+    private String validateBrand(String brand) {
+        if (brand == null || brand.trim().isEmpty()) {
+            throw new IllegalArgumentException("Марка не может быть null или пустой строкой");
+        }
+        return brand;
+    }
+
+    private int validateYear(int year) {
+        if (year < 1886) {
+            throw new IllegalArgumentException("Год выпуска не может быть ранее 1886");
+        }
+        return year;
+    }
 }
