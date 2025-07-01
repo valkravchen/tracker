243f198aa5b236c19dff31a6f7a2811cb9988219
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-01 20:13:10 +0300

    Наследование в Java

--- src/main/java/com/example/oop/Inheritance/Vehicle.java
+++ src/main/java/com/example/oop/Inheritance/Vehicle.java
@@ -30,4 +30,25 @@ public class Vehicle {
     public int getYear() {
         return year;
     }
+
+    public String getInfo() {
+        return "Транспорт: " + brand + ", год: " + year;
+    }
+
+    public String startEngine() {
+        return "Двигатель запущен";
+    }
+
+    public String stopEngine() {
+        return "Двигатель остановлен";
+    }
+
+    public static void main(String[] args) {
+        Vehicle vehicle1 = new Vehicle("мопед", 1981);
+        System.out.println(vehicle1.getBrand());
+        System.out.println(vehicle1.getYear());
+        System.out.println(vehicle1.getInfo());
+        System.out.println(vehicle1.startEngine());
+        System.out.println(vehicle1.stopEngine());
+    }
 }
