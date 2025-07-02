d8250e4eff7f67c08774f89a8d9a42a590eaf7aa
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-02 05:37:29 +0300

    Наследование в Java

--- src/main/java/com/example/oop/Inheritance/Car.java
+++ src/main/java/com/example/oop/Inheritance/Car.java
@@ -64,7 +64,7 @@ public class Car extends Vehicle {
         }
 
         try {
-            Car car2 = new Car(null, 2018, "Civic");
+            Car car2 = new Car("Nissan", 201, "Civic");
             System.out.println("Car1");
             System.out.println("getInfo: " + car2.getInfo());
             System.out.println("startEngine: " + car2.startEngine());
