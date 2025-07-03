574227ca93cb6390645ce6aa4353ea160c2f54f3
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-03 19:23:55 +0300

    Вызов метода из родительского класса

--- src/main/java/com/example/oop/Inheritance/Car.java
+++ src/main/java/com/example/oop/Inheritance/Car.java
@@ -2,9 +2,11 @@ package com.example.oop.Inheritance;
 
 public class Car extends Vehicle {
     private String model;
+    private String parentStartEngineResult;
 
     public Car(String brand, int year, String model) {
         super(brand, year);
+        this.parentStartEngineResult = super.startEngine();
         this.model = validateModel(model);
     }
 
@@ -29,14 +31,30 @@ public class Car extends Vehicle {
         return super.getInfo();
     }
 
+    public String getParentStartEngineResult() {
+        return parentStartEngineResult;
+    }
+
     public static void main(String[] args) {
         try {
             Vehicle vehicle = new Vehicle("Toyota", 2020);
             System.out.println("Vehicle:");
             System.out.println("getInfo: " + vehicle.getInfo());
             System.out.println("startEngine: " + vehicle.startEngine());
+            System.out.println();
         } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+            System.out.println("Ошибка: " + e.getMessage());
+            System.out.println();
+        }
+        try {
+            Car car1 = new Car("Honda", 2018, "Civic");
+            System.out.println("Car1:");
+            System.out.println("getInfo: " + car1.getInfo());
+            System.out.println("startEngine: " + car1.startEngine());
+            System.out.println("getParentInfo: " + car1.gerParentInfo());
+            System.out.println("super.startEngine from constructor: " + car1.getParentStartEngineResult());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка: " + e.getMessage());
             System.out.println();
         }
     }
