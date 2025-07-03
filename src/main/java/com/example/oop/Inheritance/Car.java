2d8277571e7d0326d5f3197f302e5fd88e86728c
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-03 18:18:01 +0300

    Вызов метода из родительского класса

--- src/main/java/com/example/oop/Inheritance/Car.java
+++ src/main/java/com/example/oop/Inheritance/Car.java
@@ -1,4 +1,34 @@
 package com.example.oop.Inheritance;
 
 public class Car extends Vehicle {
+    private String model;
+
+    public Car(String brand, int year, String model) {
+        super(brand, year);
+        this.model = validateModel(model);
+    }
+
+    private String validateModel(String model) {
+        if (model == null || model.trim().isEmpty()) {
+            throw new IllegalArgumentException("Модель не может быть null или пустой строкой");
+        }
+        return model;
+    }
+
+    @Override
+    public String getInfo() {
+        return "Автомобиль: " + getBrand() + ", год: " + getYear() + ", модель: " + model;
+    }
+
+    @Override
+    public String startEngine() {
+        return super.startEngine() + ", автомобиль готов";
+    }
+
+    public String gerParentInfo() {
+        return super.getInfo();
+    }
+
+    public static void main(String[] args) {
+    }
 }
