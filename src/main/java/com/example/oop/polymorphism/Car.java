a575f0ae8fa491ca469fd2b18bf3a597c2468a1d
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-05 16:55:47 +0300

    Полиморфизм

---
+++ src/main/java/com/example/oop/polymorphism/Car.java
@@ -0,0 +1,19 @@
+package com.example.oop.polymorphism;
+
+public class Car extends Vehicle {
+    private String model;
+
+    public Car(String brand, int year, String model) {
+        super(brand, year);
+        this.model = validateModel(model);
+    }
+
+
+
+    private String validateModel(String model) {
+        if (model == null || model.trim().isEmpty()) {
+            throw new IllegalArgumentException("Модель не может быть null или пустой строкой");
+        }
+        return model;
+    }
+}
