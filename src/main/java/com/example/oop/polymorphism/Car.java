994a07b0338956db001ef1b221e3f5b64e40bf96
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-05 17:24:53 +0300

    Полиморфизм

--- src/main/java/com/example/oop/polymorphism/Car.java
+++ src/main/java/com/example/oop/polymorphism/Car.java
@@ -37,6 +37,32 @@ public class Car extends Vehicle {
     }
 
     public static void main(String[] args) {
-
+        Vehicle vehicle = new Vehicle("Toyota", 2020);
+        Car car = new Car("Honda", 2021, "Civic");
+        Vehicle vehicleCar = new Car("Ford", 2022, "Mustang");
+        Vehicle[] vehicles = {
+                new Vehicle("Toyota", 2020),
+                new Car("Honda", 2021, "Civic"),
+                new Car("Ford", 2022, "Mustang")
+        };
+        System.out.println(Vehicle.getType());
+        System.out.println(vehicle.getType());
+        System.out.println(vehicle.move());
+        System.out.println(vehicle.getDescription());
+        System.out.println();
+        System.out.println(Car.getType());
+        System.out.println(car.getType());
+        System.out.println(car.move());
+        System.out.println(car.getDescription());
+        System.out.println(car.getParentMove());
+        System.out.println(car.tryOverrideFinale());
+        System.out.println();
+        System.out.println(vehicleCar.getType());
+        System.out.println(vehicleCar.move());
+        System.out.println(vehicleCar.getDescription());
+        System.out.println();
+        for (Vehicle v : vehicles) {
+            System.out.println(v.move());
+        }
     }
 }
