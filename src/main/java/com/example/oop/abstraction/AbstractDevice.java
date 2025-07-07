e2d3ed7d35eb652e3199757cad2eb019d2d1f6b9
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-07 19:44:27 +0300

    Абстракция

---
+++ src/main/java/com/example/oop/abstraction/AbstractDevice.java
@@ -0,0 +1,41 @@
+package com.example.oop.abstraction;
+
+public abstract class AbstractDevice {
+    private String name;
+    private int version;
+
+    public AbstractDevice(String name, int version) {
+        this.name = validateName(name);
+        this.version = validateVersion(version);
+    }
+
+    private String validateName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Имя не может быть null или пустой строкой");
+        }
+        return name;
+    }
+
+    public abstract String start();
+
+    public abstract String stop();
+
+    public String getStatus() {
+        return "Устройство: " + name + "; версия: " + version + "; сосояние: ";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    private int validateVersion(int version) {
+        if (version < 1) {
+            throw new IllegalArgumentException("Версия не может быть меньше 1");
+        }
+        return version;
+    }
+}
