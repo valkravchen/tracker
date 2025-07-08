676f3e3ce3e13cd2c45825b7d6ef51a4febb282d
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-08 19:17:47 +0300

    Абстракция

---
+++ src/main/java/com/example/oop/abstraction/TemperatureSensor.java
@@ -0,0 +1,48 @@
+package com.example.oop.abstraction;
+
+public class TemperatureSensor extends AbstractSensor {
+    private boolean isActive;
+
+    public TemperatureSensor(String id, int sensitivity) {
+        super(id, sensitivity);
+        this.isActive = false;
+    }
+
+    public boolean getIsActive() {
+        return isActive;
+    }
+
+    @Override
+    public String activate() {
+        isActive = true;
+        return "Температурный сенсор " + getId() + " активирован";
+    }
+
+    @Override
+    public String deactivate() {
+        isActive = false;
+        return "Температурный сенсор " + getId() + " деактивирован";
+    }
+
+    @Override
+    protected boolean isActive() {
+        return isActive;
+    }
+
+    public String measure() {
+        if (isActive) {
+            return "Температурный сенсор " + getId() + " измеряет температуру";
+        } else {
+            return "Температурный сенсор " + getId() + " неактивен, измерение невозможно";
+        }
+    }
+
+    public static void main(String[] args) {
+        TemperatureSensor temperatureSensor = new TemperatureSensor("Temp001", 50);
+        System.out.println(temperatureSensor.activate());
+        System.out.println(temperatureSensor.getStatus());
+        System.out.println(temperatureSensor.measure());
+        System.out.println(temperatureSensor.reset());
+        System.out.println(temperatureSensor.getStatus());
+    }
+}
