bb7a2b751fba84b8b0a35cb695f0074517c9b0d4
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-10 19:33:52 +0300

    Интерфейсы и абстрактные классы

---
+++ src/main/java/com/example/oop/inheritanceVSabstractClass/Car.java
@@ -0,0 +1,23 @@
+package com.example.oop.inheritanceVSabstractClass;
+
+public class Car extends AbstractVehicle implements Movable {
+    private int speed;
+
+    public Car(String brand, int year) {
+        super(brand, year);
+    }
+
+    @Override
+    public String move() {
+        return "Автомобиль " + getBrand() + " года " + getYear() + " движется со скоростью " + speed + "км/ч";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Автомобиль " + getBrand() + " ускоряется до " + speed + "км/ч";
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
