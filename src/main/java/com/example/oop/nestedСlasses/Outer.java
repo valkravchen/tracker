1f92c01559b93b6825ba558b9251f007a6da4120
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-11 18:23:33 +0300

    Внутренние классы

---
+++ src/main/java/com/example/oop/nestedСlasses/Outer.java
@@ -0,0 +1,33 @@
+package com.example.oop.nestedСlasses;
+
+public class Outer {
+    private final String name;
+    private final int id;
+
+    public Outer(String name, int id) {
+        this.name = validateName(name);
+        this.id = validateId(id);
+    }
+
+    private String validateName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Имя не может быть null или пустой строкой");
+        }
+        return name.trim();
+    }
+
+    private int validateId(int id) {
+        if (id < 1) {
+            throw new IllegalArgumentException("ID не может быть меньше 1");
+        }
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
