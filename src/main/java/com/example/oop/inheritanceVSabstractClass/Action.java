4cc4451e4d49360e55f9a8f1e96c6001f42d9917
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-12 05:25:08 +0300

    Внутренние классы

--- src/main/java/com/example/oop/inheritanceVSabstractClass/Action.java
+++
@@ -1,5 +0,0 @@
-package com.example.oop.inheritanceVSabstractClass;
-
-public interface Action {
-    String execute();
-}
--- src/main/java/com/example/oop/inheritanceVSabstractClass/Movable.java
+++
@@ -1,9 +0,0 @@
-package com.example.oop.inheritanceVSabstractClass;
-
-public interface Movable {
-    public abstract String move();
-
-    public default String accelerate() {
-        return "Объект ускоряется по умолчанию";
-    }
-}
