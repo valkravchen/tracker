a5b8cceb147410f43005d52100e624b6b56f7742
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-12 17:04:11 +0300

    Внутренние классы

--- src/main/java/com/example/oop/nestedClasses/DataProcessor.java
+++ src/main/java/com/example/oop/nestedClasses/DataProcessor.java
@@ -2,9 +2,11 @@ package com.example.oop.nestedClasses;
 
 public class DataProcessor {
     private final String processorName;
+    private final int id;
 
-    public DataProcessor(String processorName) {
+    public DataProcessor(String processorName, int id) {
         this.processorName = validateProcessorName(processorName);
+        this.id = validateId(id);
     }
 
     private String validateProcessorName(String processorName) {
@@ -14,8 +16,19 @@ public class DataProcessor {
         return processorName;
     }
 
+    private int validateId(int id) {
+        if (id < 1) {
+            throw new IllegalArgumentException("ID не может быть меньше 1");
+        }
+        return id;
+    }
+
     public String getProcessorName() {
         return processorName;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public static class Utility {
@@ -35,8 +48,28 @@ public class DataProcessor {
         }
     }
 
-    public static void main(String[] args) {
-        System.out.println(DataProcessor.Utility.formatData("test data"));
-        System.out.println(DataProcessor.Constans.getDefaultFormat());
+    public static class Builder {
+        private String tempName;
+        private int tempId;
+
+        public Builder setTempName(String tempName) {
+            if (tempName == null || tempName.trim().isEmpty()) {
+                throw new IllegalArgumentException("Имя не может быть null или пустой строкой");
+            }
+            return this;
+        }
+
+        public Builder setTempId(int tempId) {
+            if (tempId < 1) {
+                throw new IllegalArgumentException("ID не может быть меньше 1");
+            }
+            return this;
+        }
+
+
+        public static void main(String[] args) {
+            System.out.println(DataProcessor.Utility.formatData("test data"));
+            System.out.println(DataProcessor.Constans.getDefaultFormat());
+        }
     }
 }
