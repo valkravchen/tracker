8e97e6e736ada45a2ce70e3520d3434d6b957b62
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-12 04:21:16 +0300

    Внутренние классы

---
+++ src/main/java/com/example/oop/inheritanceVSabstractClass/Container.java
@@ -0,0 +1,20 @@
+package com.example.oop.inheritanceVSabstractClass;
+
+public class Container {
+    private final int capacity;
+
+    public Container(int capacity) {
+        this.capacity = validateCapacity(capacity);
+    }
+
+    private int validateCapacity(int capacity) {
+        if (capacity < 1) {
+            throw new IllegalArgumentException("Ёмкость не может быть меньше 1");
+        }
+        return capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
