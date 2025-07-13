fb56936e515c0ad0cf1d1b6711ea0fc7c65a84f5
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-13 16:35:04 +0300

    4. Что такое конструктор?

--- src/main/java/com/example/oop/constructor/Book.java
+++ src/main/java/com/example/oop/constructor/Book.java
@@ -1,4 +1,22 @@
 package com.example.oop.constructor;
 
 public class Book {
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getInfo() {
+        return "Книга: " + title + ", автор: " + author;
+    }
+
+    public static void main(String[] args) {
+        Book book1 = new Book("Преступление и наказание", "Ф.М. Достоевский");
+        Book book2 = new Book("Мастер и Маргарита", "М.А. Булгаков");
+        System.out.println(book1.getInfo());
+        System.out.println(book2.getInfo());
+    }
 }
