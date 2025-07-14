2a56980e19904a1a6eb6c6c1e073ad9f04adaf46
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-14 18:30:35 +0300

    7. Что такое статический метод?

---
+++ src/main/java/com/example/oop/staticMethod/Greeting.java
@@ -0,0 +1,11 @@
+package com.example.oop.staticMethod;
+
+public class Greeting {
+    public static void sayHello() {
+        System.out.println("Привет");
+    }
+
+    public static void main(String[] args) {
+        Greeting.sayHello();
+    }
+}
