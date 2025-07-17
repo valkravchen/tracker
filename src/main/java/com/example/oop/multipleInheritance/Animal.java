4399ec7493cfcdb86f10dd6db9bd87a034e6cd10
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-17 05:35:29 +0300

    21. Что такое полиморфизм?

---
+++ src/main/java/com/example/oop/multipleInheritance/Animal.java
@@ -0,0 +1,7 @@
+package com.example.oop.multipleInheritance;
+
+public class Animal {
+    public void speak() {
+        System.out.println("Животное издаёт звук");
+    }
+}
---
+++ src/main/java/com/example/oop/multipleInheritance/Cat.java
@@ -0,0 +1,13 @@
+package com.example.oop.multipleInheritance;
+
+public class Cat extends Animal{
+    @Override
+    public void speak() {
+        System.out.println("Кошка мяукает");
+    }
+
+    public static void main(String[] args) {
+        Animal animal = new Cat();
+        animal.speak();
+    }
+}
