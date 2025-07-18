9452195965721c769ed41b63b599cee4b3252d38
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-18 04:59:00 +0300

    23. Как реализована инкапсуляция в Java?

--- src/main/java/com/example/oop/encapsulation/Temperature.java
+++ src/main/java/com/example/oop/encapsulation/Temperature.java
@@ -1,11 +1,21 @@
 package com.example.oop.encapsulation;
 
 public class Temperature {
-    public double celsius;
+    private double celsius;
+
+    public void setCelsius(double c) {
+        if (c > -273.15) {
+            this.celsius = c;
+        }
+    }
+
+    public double getCelsius() {
+        return celsius;
+    }
 
     public static void main(String[] args) {
         Temperature temperature = new Temperature();
-        temperature.celsius = -100;
-        System.out.println(temperature.celsius);
+        temperature.setCelsius(30);
+        System.out.println(temperature.getCelsius());
     }
 }
