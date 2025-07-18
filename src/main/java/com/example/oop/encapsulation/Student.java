8e63712ea684274496c84268cda9670e3c39da54
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-18 20:10:19 +0300

    23. Как реализована инкапсуляция в Java?

---
+++ src/main/java/com/example/oop/encapsulation/Student.java
@@ -0,0 +1,19 @@
+package com.example.oop.encapsulation;
+
+public class Student {
+    private String name;
+    private int age;
+    private double gpa;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+}
