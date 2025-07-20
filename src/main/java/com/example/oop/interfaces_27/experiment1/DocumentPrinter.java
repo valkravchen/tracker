989bc6a17d62fcdf218b4ae4bbb4ced5ec1ce3b1
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-20 14:09:02 +0300

    27. Можно ли создать объект интерфейса? Если да, то как? Эксперимент 1: Создание через реализующий класс
    Цель: Показать базовый способ создания объектов интерфейса через реализующий класс.

--- src/main/java/com/example/oop/interfaces_27/experiment1/DocumentPrinter.java
+++ src/main/java/com/example/oop/interfaces_27/experiment1/DocumentPrinter.java
@@ -1,11 +1,12 @@
 package com.example.oop.interfaces_27.experiment1;
 
-public class DocumentPrinter implements Printable{
+public class DocumentPrinter implements Printable {
     private String printName;
 
     public DocumentPrinter(String printName) {
         this.printName = printName;
     }
+
     @Override
     public void print(String text) {
         System.out.println("[" + printName + "] Печать: " + text);
