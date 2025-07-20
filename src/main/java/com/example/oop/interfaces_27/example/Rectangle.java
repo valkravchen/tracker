e5f43ee002b25e51a3c3f28907e88f28b4d4d59f
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-20 13:29:57 +0300

    27. Можно ли создать объект интерфейса? Если да, то как?

---
+++ src/main/java/com/example/oop/interfaces_27/example/Rectangle.java
@@ -0,0 +1,15 @@
+package com.example.oop.interfaces_27.example;
+
+public class Rectangle implements Shape {
+    private double width, height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+}
