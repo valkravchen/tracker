be84758b783cfc404cc34dbdb228b9b07bbc0056
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-20 06:37:45 +0300

    26. Перечислите элементы, которые может содержать интерфейс.  Эксперимент 1: Интерфейс с константами и абстрактными методами

---
+++ src/main/java/com/example/oop/interfaces/experiment1/MathConstants.java
@@ -0,0 +1,11 @@
+package com.example.oop.interfaces.experiment1;
+
+public interface MathConstants {
+    double PI = 3.14159;
+    double E = 2.71828;
+    int MAX_ITERATIONS = 100;
+
+    double calculate();
+
+    String getFormula();
+}
