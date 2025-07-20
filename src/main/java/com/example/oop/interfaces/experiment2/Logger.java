ca1b41dd22f6e17053d398dd00c1fb8d9c2f9085
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-20 07:29:40 +0300

    26. Перечислите элементы, которые может содержать интерфейс. Эксперимент 2: Default-методы в интерфейсе. Цель: Показать использование default-методов для предоставления реализации по умолчанию.

---
+++ src/main/java/com/example/oop/interfaces/experiment2/Logger.java
@@ -0,0 +1,17 @@
+package com.example.oop.interfaces.experiment2;
+
+public interface Logger {
+    void log(String message);
+
+    default void info(String message) {
+        log("INFO: " + message);
+    }
+
+    default void error(String message) {
+        log("ERROR: " + message);
+    }
+
+    default void debug(String message) {
+        log("DEBUG: " + message);
+    }
+}
