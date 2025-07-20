b8e2aa765824ba47fe2de67723db17cbcdbd77af
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-20 19:14:13 +0300

    28. Может ли интерфейс содержать какие-либо методы с реализацией? Если да, то какие?

---
+++ src/main/java/com/example/oop/interfaces_28/example/Adder.java
@@ -0,0 +1,8 @@
+package com.example.oop.interfaces_28.example;
+
+public class Adder implements Calculator {
+    @Override
+    public double calculate(double a, double b) {
+        return a + b;
+    }
+}
