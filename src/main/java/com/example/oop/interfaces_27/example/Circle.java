ad4b864c2b0de5035bd6d177a14b681af0a514ac
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-20 13:26:30 +0300

    27. Можно ли создать объект интерфейса? Если да, то как?

---
+++ src/main/java/com/example/oop/interfaces_27/example/Circle.java
@@ -0,0 +1,14 @@
+package com.example.oop.interfaces_27.example;
+
+public class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
---
+++ src/main/java/com/example/oop/interfaces_27/example/Shape.java
@@ -0,0 +1,5 @@
+package com.example.oop.interfaces_27.example;
+
+public interface Shape {
+    double getArea();
+}
