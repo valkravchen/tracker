4d7d844efad128dcbff068385e2a32724b90c69d
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-20 17:08:26 +0300

    27. Можно ли создать объект интерфейса? Если да, то как? Эксперимент 6: Default методы и их переопределение
    Цель: Показать работу с default методами при создании объектов интерфейса.

---
+++ src/main/java/com/example/oop/interfaces_27/experiment6/Logger.java
@@ -0,0 +1,39 @@
+package com.example.oop.interfaces_27.experiment6;
+
+public interface Logger {
+    void log(String message);
+
+    default void info(String msg) {
+        log("INFO: " + msg);
+    }
+
+    default void error(String msg) {
+        log("ERROR: " + msg);
+    }
+
+    public static void main(String[] args) {
+        Logger consoleLogger = new Logger() {
+            @Override
+            public void log(String message) {
+                System.out.println(message);
+            }
+        };
+        Logger prefixLogger = new Logger() {
+            @Override
+            public void log(String message) {
+                System.out.println(message);
+            }
+            @Override
+            public void info(String msg){
+                log(">>> " + msg);
+            }
+        };
+        System.out.println("=== Console Logger ===");
+        consoleLogger.info("Запуск приложения");
+        consoleLogger.error("Ошибка подключения");
+        System.out.println();
+        System.out.println("=== Prefix Logger ===");
+        prefixLogger.info("Запуск приложения");
+        prefixLogger.error("Ошибка подключения");
+    }
+}
