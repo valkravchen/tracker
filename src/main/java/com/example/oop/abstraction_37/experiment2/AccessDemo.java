84d8e9506ccabd35d74a5881f6b5a5594dea0941
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-23 18:13:18 +0300

    37. Какие типы вложенных классов существуют в Java?
    Эксперимент 2: Различия в доступе к членам внешнего класса
    
    Цель: Показать какие типы вложенных классов имеют доступ к каким членам внешнего класса

---
+++ src/main/java/com/example/oop/abstraction_37/experiment2/AccessDemo.java
@@ -0,0 +1,39 @@
+package com.example.oop.abstraction_37.experiment2;
+
+public class AccessDemo {
+    private String instanceField = "экземпляр";
+    private static String staticField = "статическое";
+
+    static class StaticAccessor {
+        public void showAccess() {
+            System.out.println("Static доступ: " + staticField);
+        }
+    }
+
+    class InstanceAccessor {
+        public void showAccess() {
+            System.out.println("Instance доступ: " + instanceField + " и " + staticField);
+        }
+    }
+
+    public void demonstrateLocalAccess(String parameter) {
+        String localVar = "локальная";
+        class LocalAccessor {
+            public void showAccess() {
+                System.out.println("Local доступ: " + parameter + ", "
+                        + localVar + ", " + instanceField + ", " + staticField);
+            }
+        }
+        LocalAccessor localAccessor = new LocalAccessor();
+        localAccessor.showAccess();
+    }
+
+    public static void main(String[] args) {
+        AccessDemo.StaticAccessor staticAccessor = new StaticAccessor();
+        staticAccessor.showAccess();
+        AccessDemo accessDemo = new AccessDemo();
+        AccessDemo.InstanceAccessor instanceAccessor = accessDemo.new InstanceAccessor();
+        instanceAccessor.showAccess();
+        accessDemo.demonstrateLocalAccess("параметр");
+    }
+}
