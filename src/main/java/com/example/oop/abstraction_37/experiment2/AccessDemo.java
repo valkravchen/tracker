2bc23a263f64671244883975b56130b858ec648d
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-23 18:14:50 +0300

    37. Какие типы вложенных классов существуют в Java?
    Эксперимент 2: Различия в доступе к членам внешнего класса
    
    Цель: Показать какие типы вложенных классов имеют доступ к каким членам внешнего класса

--- src/main/java/com/example/oop/abstraction_37/experiment2/AccessDemo.java
+++ src/main/java/com/example/oop/abstraction_37/experiment2/AccessDemo.java
@@ -24,6 +24,7 @@ public class AccessDemo {
                         + localVar + ", " + instanceField + ", " + staticField);
             }
         }
+
         LocalAccessor localAccessor = new LocalAccessor();
         localAccessor.showAccess();
     }
