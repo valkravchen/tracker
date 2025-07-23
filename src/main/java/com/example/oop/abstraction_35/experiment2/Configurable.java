b878bdf936d8c1bbd2b213c2f09ca23084c5915e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-23 04:09:33 +0300

    35. Чем отличается интерфейс от абстрактного класса?
    Эксперимент 2: Default, static и private методы в интерфейсах vs конкретные методы и поля в абстрактных классах
    
    Цель: Показать различия в предоставлении готовой функциональности, работе с константами и полями

---
+++ src/main/java/com/example/oop/abstraction_35/experiment2/Configurable.java
@@ -0,0 +1,20 @@
+package com.example.oop.abstraction_35.experiment2;
+
+public interface Configurable {
+    String DEFAULT_FORMAT = "JSON";
+
+    String getConfigValue(String key);
+
+    default void printConfig(String key) {
+        System.out.println("Конфигурация " + key + ": результат " + getConfigValue(key));
+        logAccess(key);
+    }
+
+    static String getDefaultFormat() {
+        return DEFAULT_FORMAT;
+    }
+
+    private void logAccess(String key) {
+        System.out.println("Лог: обращение к ключу " + key);
+    }
+}
