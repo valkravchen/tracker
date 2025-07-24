b7dc85bf1b64b0549fbcdeb1f2c94b2faba54792
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-24 05:39:32 +0300

    37. Какие типы вложенных классов существуют в Java?
    Эксперимент 3: Эволюция static элементов в разных типах вложенных классов
    
    Цель: Показать современные возможности static элементов и различия между типами вложенных классов

---
+++ src/main/java/com/example/oop/nested_37/experiment3/EvolutionDemo.java
@@ -0,0 +1,46 @@
+package com.example.oop.nested_37.experiment3;
+
+public class EvolutionDemo {
+    static class StaticNested {
+        static int staticCounter = 0;
+
+        static void incrementStatic() {
+            staticCounter++;
+            System.out.println("Статический-вложенный счетчик: " + staticCounter);
+        }
+
+        public void showType() {
+            System.out.println("Тип: статический-вложенный-класс");
+        }
+    }
+
+    class Inner {
+        static final String TYPE = "внутренний-класс";
+        static int innerCounter = 0;
+
+        public void showType() {
+            System.out.println("Тип: " + TYPE);
+        }
+    }
+
+    public void createLocalClass() {
+        class LocalClass {
+            static final String SCOPE = "локальный-класс";
+            static int localCounter = 0;
+
+            static public void incrementLocal() {
+                System.out.println("Локальный счетчик: " + localCounter);
+            }
+
+            public void showType() {
+                System.out.println("Тип: " + SCOPE);
+            }
+        }
+
+        LocalClass.incrementLocal();
+        LocalClass localClass = new LocalClass();
+        localClass.showType();
+    }
+
+
+}
