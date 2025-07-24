0480875b589dcc3165be3e9da6f7cbb55b20ec4e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-24 04:38:08 +0300

    37. Какие типы вложенных классов существуют в Java?
    Эксперимент 1: Создание и использование всех четырех типов вложенных классов
    
    Цель: Продемонстрировать синтаксис объявления и создания объектов каждого типа

---
+++ src/main/java/com/example/oop/nested_37/experiment1/Company.java
@@ -0,0 +1,67 @@
+package com.example.oop.nested_37.experiment1;
+
+public class Company {
+    private String companyName = "ТехКорп";
+    private static String industry = "IT-отрасль";
+
+    static class Department {
+        private String name;
+
+        public Department(String name) {
+            this.name = name;
+        }
+
+        public void showInfo() {
+            System.out.println("Статический вложенный: " + name + " в " + industry);
+        }
+    }
+
+    class Employee {
+        String employeeName;
+
+        public Employee(String employeeName) {
+            this.employeeName = employeeName;
+        }
+
+        public void introduce() {
+            System.out.println("Внутренний класс: " + employeeName + " работает в " + companyName);
+        }
+    }
+
+    public void processApplications() {
+        String status = "Локальный статус";
+        class Application {
+            private String applicantName;
+
+            public Application(String applicantName) {
+                this.applicantName = applicantName;
+            }
+
+            public void process() {
+                System.out.println("Локальный класс: заявка от " + applicantName + " со статусом " + status);
+            }
+        }
+
+        Application application = new Application("Иван Петров");
+        application.process();
+    }
+
+    public void createTask(String taskName) {
+        interface Task {
+            void execute();
+        }
+
+        Task task = new Task() {
+            @Override
+            public void execute() {
+                System.out.println("Анонимный класс: выполняю " + taskName);
+            }
+        };
+
+        task.execute();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
