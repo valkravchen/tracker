e17deca40b49cde9bc5559e290721c4c83d3d1cd
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-24 19:56:45 +0300

    41. Что такое локальный класс? Когда он применяется?

---
+++ src/main/java/com/example/oop/localClasses_41/experiment1/ScopeAccessDemo.java
@@ -0,0 +1,6 @@
+package com.example.oop.localClasses_41.experiment1;
+
+public class ScopeAccessDemo {
+    private String instanceField = "поле-экземпляра-внешнего";
+    private static String staticField = "статическое-поле-внешнего";
+}
