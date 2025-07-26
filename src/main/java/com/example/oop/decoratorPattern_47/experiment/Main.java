369190d913badf72debd55d646189d8e37576b81
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-26 06:30:09 +0300

    47. Шаблон "Декоратор"
    ЗАДАЧА: "Добавить поддержку Slack уведомлений во все существующие комбинации"
    
    ДЕМОНСТРАЦИЯ ПРОЦЕССА 1:

--- src/main/java/com/example/oop/decoratorPattern_47/experiment/Main.java
+++ src/main/java/com/example/oop/decoratorPattern_47/experiment/Main.java
@@ -5,5 +5,18 @@ public class Main {
         System.out.println("=== ПРОЦЕСС 1: РЕШЕНИЕ БЕЗ ПАТТЕРНА ===");
         System.out.println("ЗАДАЧА: Добавить Slack во все комбинации");
         System.out.println("ШАГ 1: Анализ - нужны классы: Slack, EmailSlack, SMSSlack, EmailSMSSlack");
+        System.out.println("ШАГ 2: Создаю SlackNotificationV1");
+        SlackNotificationV1 slack = new SlackNotificationV1();
+        slack.send("тест-slack");
+        System.out.println("ШАГ 3: Создаю EmailSlackNotificationV");
+        EmailSlackNotificationV1 emailSlack = new EmailSlackNotificationV1();
+        emailSlack.send("тест-email-slack");
+        System.out.println("ШАГ 4: Создаю SMSSlackNotificationV1");
+        SMSSlackNotificationV1 smsSlack = new SMSSlackNotificationV1();
+        smsSlack.send("тест-sms-slack");
+        System.out.println("ШАГ 5: Создаю EmailSMSSlackNotificationV1");
+        EmailSMSSlackNotificationV1 emailSMSSlack = new EmailSMSSlackNotificationV1();
+        emailSMSSlack.send("тест-все-типы");
+        System.out.println("РЕЗУЛЬТАТ ПРОЦЕССА 1: Было 4 класса → стало 7 классов");
     }
 }
