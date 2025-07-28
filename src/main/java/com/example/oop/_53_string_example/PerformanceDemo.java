8fbe324398fd02fefff794054ed9a4a7a46c5614
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-07-28 04:53:14 +0300

    53. Зачем нужны StringBuilder, StringBuffer, StringJoiner? Что они делают?

--- src/main/java/com/example/oop/_53_string_example/PerformanceDemo.java
+++ src/main/java/com/example/oop/_53_string_example/PerformanceDemo.java
@@ -5,13 +5,13 @@ public class PerformanceDemo {
         System.out.println("Тест производительности для " + iterations + " итераций:");
         long startTime = System.currentTimeMillis();
         String result1 = "";
-        for (int i = 0; i < iterations ; i++) {
+        for (int i = 0; i < iterations; i++) {
             result1 += "строка-" + i + " ";
         }
         long stringTime = System.currentTimeMillis() - startTime;
         stringTime = System.currentTimeMillis();
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < iterations ; i++) {
+        for (int i = 0; i < iterations; i++) {
             stringBuilder.append("строка-").append(i).append(" ");
         }
         String result2 = stringBuilder.toString();
