28f9493eec1fabf78ce80a19a9e0aafb1fa08a43
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-08-01 18:58:50 +0300

    Паттерн "Декоратор"

---
+++ src/main/java/com/example/oop/patterns_decorator/MushroomsDecorator.java
@@ -0,0 +1,17 @@
+package com.example.oop.patterns_decorator;
+
+public class MushroomsDecorator extends PizzaDecorator{
+    public MushroomsDecorator(Pizza pizza) {
+        super(pizza);
+    }
+
+    @Override
+    public int cost() {
+        return pizza.cost() + 80;
+    }
+
+    @Override
+    public String description() {
+        return pizza.description() + ", грибы";
+    }
+}
