55b47afc798f856e1beb24aaf34baabe005f62b7
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-08-04 05:15:05 +0300

    13.4. Телефонный справочник

--- src/test/java/ru/search/PhoneDictionaryTest.java
+++ src/test/java/ru/search/PhoneDictionaryTest.java
@@ -18,4 +18,15 @@ class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Ivan");
         assertThat(persons.get(0).getSurname()).isEqualTo("Ivanov");
     }
+
+    @Test
+    public void whenFindByNameNotFound() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Ivan", "Ivanov", "534872", "Bryansk")
+        );
+
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons).isEmpty();
+    }
 }
