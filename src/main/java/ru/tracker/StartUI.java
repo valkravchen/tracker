8d30e1e32121d7ce25908774fc84d59184c07b0a
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-08-10 21:25:43 +0300

    13.9. Tracker

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -3,7 +3,6 @@ package ru.tracker;
 import ru.tracker.action.*;
 import ru.tracker.input.ConsoleInput;
 import ru.tracker.input.Input;
-
 import ru.tracker.input.ValidateInput;
 import ru.tracker.output.ConsoleOutput;
 import ru.tracker.output.Output;
