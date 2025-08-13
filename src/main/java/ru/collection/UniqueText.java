7e022a7ff91b36c4cee184330c8d8964186b3cde
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-08-13 20:29:17 +0300

    14.3. Идентичные тексты

---
+++ src/main/java/ru/collection/UniqueText.java
@@ -0,0 +1,23 @@
+package ru.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        if (origin.length != text.length) {
+            return false;
+        }
+        HashSet<String> check = new HashSet<>();
+        for (String word : origin) {
+            check.add(word);
+        }
+        for (String word : text) {
+            if (!check.contains(word)) {
+             return false;
+            }
+        }
+        return true;
+    }
+}
