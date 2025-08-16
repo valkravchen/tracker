92b1398b069b4076de8ac945580d61a8e352be43
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-08-16 20:09:45 +0300

    14.4 Модели данных и HashSet

--- src/main/java/ru/collection/Account.java
+++ src/main/java/ru/collection/Account.java
@@ -6,6 +6,7 @@ public class Account {
     private String passport;
     private String username;
     private String deposit;
+
     public Account(String passport, String username, String deposit) {
         this.passport = passport;
         this.username = username;
