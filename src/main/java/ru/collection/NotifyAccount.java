70a73da5737355790b547e2d4b9b2dda6eae761b
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-08-21 19:40:46 +0300

    14.4 Модели данных и HashSet

--- src/main/java/ru/collection/NotifyAccount.java
+++ src/main/java/ru/collection/NotifyAccount.java
@@ -2,9 +2,10 @@ package ru.collection;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class NotifyAccount {
-    public static HashSet<Account> sent(List<Account> accounts) {
+    public static Set<Account> sent(List<Account> accounts) {
         return new HashSet<>(accounts);
     }
 }
