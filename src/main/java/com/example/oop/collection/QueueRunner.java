96f90def29f1f79c408a6ec8cd1cb4e5f8ab2c12
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-08-22 21:12:20 +0300

    15.1 Интерфейс Queue в Java

--- src/main/java/com/example/oop/collection/QueueRunner.java
+++ src/main/java/com/example/oop/collection/QueueRunner.java
@@ -6,18 +6,20 @@ import java.util.Queue;
 public class QueueRunner {
     public static void main(String[] args) {
         Queue<String> queue = new LinkedList<>();
-        queue.add("first");
-        queue.add("second");
-        queue.add("third");
-
-        for (String string: queue) {
-            System.out.println(string);
-        }
-        queue.remove();
-        System.out.println();
-        System.out.println("State of Queue after remove: ");
-        for (String string : queue) {
-            System.out.println(string);
-        }
+        String temp = queue.poll();
+        System.out.println(temp);
+//        queue.add("first");
+//        queue.add("second");
+//        queue.add("third");
+//
+//        for (String string: queue) {
+//            System.out.println(string);
+//        }
+//        queue.remove();
+//        System.out.println();
+//        System.out.println("State of Queue after remove: ");
+//        for (String string : queue) {
+//            System.out.println(string);
+//        }
     }
 }
