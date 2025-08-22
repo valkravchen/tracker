61ad0d4e2189cdb20ce639b6656f39efd43fa4a2
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-08-22 20:34:24 +0300

    15.1 Интерфейс Queue в Java

---
+++ src/main/java/com/example/oop/collection/QueueRunner.java
@@ -0,0 +1,17 @@
+package com.example.oop.collection;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueRunner {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+        queue.add("first");
+        queue.add("second");
+        queue.add("third");
+
+        for (String string: queue) {
+            System.out.println(string);
+        }
+    }
+}
