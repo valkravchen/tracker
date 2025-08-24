1a9ace2ca8b6970d61ba52746d6ed4b5ba8f895d
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-08-24 16:31:21 +0300

    15.2 Deque

---
+++ src/main/java/ru/queue/ReconstructPhrase.java
@@ -0,0 +1,37 @@
+package ru.queue;
+
+import java.util.Deque;
+
+public class ReconstructPhrase {
+    private final Deque<Character> descendingElements;
+    private final Deque<Character> evenElements;
+
+    public ReconstructPhrase(Deque<Character> descendingElements,
+                             Deque<Character> evenElements) {
+        this.descendingElements = descendingElements;
+        this.evenElements = evenElements;
+    }
+
+    private String getEvenElements() {
+        StringBuilder sb = new StringBuilder();
+        int originalSize = evenElements.size();
+        for (int index = 0; index < originalSize/2; index ++) {
+                sb.append(evenElements.pollFirst());
+                evenElements.pollFirst();
+        }
+        return sb.toString();
+    }
+
+    private String getDescendingElements() {
+        StringBuilder sb = new StringBuilder();
+        int originalSize = descendingElements.size();
+        for (int index = 0; index < originalSize; index++) {
+            sb.append(descendingElements.pollLast());
+        }
+        return sb.toString();
+    }
+
+    public String getReconstructPhrase() {
+        return getEvenElements() + getDescendingElements();
+    }
+}
