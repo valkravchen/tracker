26bc02ea60f78aab402727de29288fa71dca25ac
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-08-24 13:01:51 +0300

    15.2 Deque

--- src/main/java/ru/queue/Main.java
+++ src/main/java/ru/queue/Main.java
@@ -1,4 +1,18 @@
 package ru.queue;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Main {
+    public static void main(String[] args) {
+        Deque<String> deque = new LinkedList<>();
+        deque.add("second");
+        deque.addFirst("first");
+        deque.addLast("third");
+        System.out.println(deque);
+        System.out.println(deque.pop());
+        System.out.println(deque.poll());
+        System.out.println(deque.pollFirst());
+        System.out.println(deque.pollLast());
+    }
 }
