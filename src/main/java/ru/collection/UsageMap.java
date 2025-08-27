bac747487599edba75fadcc680b8dac8ae1b4598
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-08-27 20:20:27 +0300

    15.3. Map, HashMap

--- src/main/java/ru/collection/UsageMap.java
+++ src/main/java/ru/collection/UsageMap.java
@@ -5,8 +5,9 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
-        map.put("qwerty@yandex.ru", "Ivan Ivanov");
-
+        map.put("ivanov@yandex.ru", "Иванов Иван Иванович");
+        map.put("petrov@yandex.ru", "Петров Пётр Петрович");
+        map.put("alekseev@yandex.ru", "Алексеев Алексей Алексеевич");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
