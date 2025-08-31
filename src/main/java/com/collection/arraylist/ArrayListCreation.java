1c1aa035d757646de3c234b8b8211734e718cb8c
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-08-31 10:04:09 +0300

    ArrayList

---
+++ src/main/java/com/collection/arraylist/ArrayListCreation.java
@@ -0,0 +1,11 @@
+package com.collection.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListCreation {
+    public static void main(String[] args) {
+        List<String> list1 = new ArrayList<>();
+        System.out.println("Пустой список: " + list1);
+    }
+}
