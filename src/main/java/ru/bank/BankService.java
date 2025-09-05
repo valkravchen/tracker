c5d83bd6a866483adeaf00c813b7bb3881464010
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-09-05 20:14:27 +0300

    Map->HashMap: подсчёт слов

---
+++ src/main/java/ru/bank/BankService.java
@@ -0,0 +1,38 @@
+package ru.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void deleteUser(String passport) {
+    }
+
+    public void addAccount(String passport, Account account) {
+    }
+
+    public User findByPassport(String passport) {
+        return null;
+    }
+
+    public Account findByRequisite() {
+        return null;
+    }
+
+    public boolean transferMoney(String sourcePassport, String sourceRequisite, String destinationPassport,
+                                 String destinationRequisite, double amount) {
+        boolean result = false;
+        return result;
+    }
+
+    public List<Account> getAccounts(User user) {
+        return users.get(user);
+    }
+}
