3a9a209bbb3a7c666393515833a2eee8d8bfb188
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-09-06 15:37:40 +0300

    16.4 Банковские переводы findByPassport

--- src/main/java/ru/bank/BankService.java
+++ src/main/java/ru/bank/BankService.java
@@ -13,12 +13,18 @@ public class BankService {
     }
 
     public void deleteUser(String passport) {
+        users.remove(passport);
     }
 
     public void addAccount(String passport, Account account) {
     }
 
     public User findByPassport(String passport) {
+        for(User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
         return null;
     }
 
@@ -26,12 +32,27 @@ public class BankService {
         return null;
     }
 
-    public boolean transferMoney(String sourcePassport, String sourceRequisite, String destinationPassport,
-                                 String destinationRequisite, double amount) {
+    public boolean transferMoney(String sourcePassport, String sourceRequisite,
+                                 String destinationPassport, String destinationRequisite, double amount) {
+
         return false;
     }
 
     public List<Account> getAccounts(User user) {
         return users.get(user);
+    }
+
+    public static void main(String[] args) {
+        BankService service = new BankService();
+        User ivan = new User("1234", "Иван");
+        User maria = new User("5678", "Мария");
+        service.users.put(ivan, new ArrayList<>());
+        service.users.put(maria, new ArrayList<>());
+        User found1 = service.findByPassport("1234");
+        User found2 = service.findByPassport("5678");
+        User found3 = service.findByPassport("9999");
+        System.out.println(found1 != null ? found1.getUsername() : null);
+        System.out.println(found1 != null ? found2.getUsername() : null);
+        System.out.println(found3);
     }
 }
