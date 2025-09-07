f989fcd67d674fbb53f3535a3728e03a3f231885
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-09-07 19:20:24 +0300

    16.5 Аттестация

--- src/main/java/ru/hashmap/AnalyzeByMap.java
+++ src/main/java/ru/hashmap/AnalyzeByMap.java
@@ -4,7 +4,15 @@ import java.util.List;
 
 public class AnalyzeByMap {
     public static double averageScore(List<Pupil> pupils) {
-        return 0D;
+        int sum = 0;
+        int count = 0;
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                sum += subject.score();
+                count++;
+            }
+        }
+        return (double) sum / count;
     }
 
     public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
@@ -21,5 +29,31 @@ public class AnalyzeByMap {
 
     public static Label bestSubject(List<Pupil> pupils) {
         return null;
+    }
+
+    public static void main(String[] args) {
+        List<Pupil> pupils = List.of(
+                new Pupil("Ivanov",
+                        List.of(
+                                new Subject("Math", 70),
+                                new Subject("Lang", 90),
+                                new Subject("Philosophy", 100)
+                        )
+                ),
+                new Pupil("Petrov",
+                        List.of(
+                                new Subject("Lang", 60),
+                                new Subject("Math", 60),
+                                new Subject("Philosophy", 60)
+                        )
+                ),
+                new Pupil("Sidorov",
+                        List.of(
+                                new Subject("Philosophy", 50),
+                                new Subject("Lang", 60),
+                                new Subject("Math", 80)
+                        )
+                )
+        );
     }
 }
---
+++ src/test/java/ru/hashmap/AnalyzeByMapTest.java
@@ -0,0 +1,40 @@
+package ru.hashmap;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class AnalyzeByMapTest {
+
+    @Test
+    public void whenAverageScore() {
+        double average = AnalyzeByMap.averageScore(
+                List.of(
+                        new Pupil("Ivanov",
+                                List.of(
+                                        new Subject("Math", 100),
+                                        new Subject("Lang", 70),
+                                        new Subject("Philosophy", 80)
+                                )
+                        ),
+                        new Pupil("Petrov",
+                                List.of(
+                                        new Subject("Math", 80),
+                                        new Subject("Lang", 90),
+                                        new Subject("Philosophy", 70)
+                                )
+                        ),
+                        new Pupil("Sidorov",
+                                List.of(
+                                        new Subject("Math", 70),
+                                        new Subject("Lang", 60),
+                                        new Subject("Philosophy", 50)
+                                )
+                        )
+                )
+        );
+        assertThat(average).isCloseTo(74.44, offset(0.01D));
+    }
+}
