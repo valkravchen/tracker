546cec878204a01eb666db1c046d936439198399
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-09-17 19:58:41 +0300

    17.1 Сортировка

---
+++ src/main/java/ru/collection/ListSort.java
@@ -0,0 +1,11 @@
+package ru.collection;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListSort {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(5, 3, 4, 1, 2);
+        System.out.println(list);
+    }
+}
