9d7e3ed9924ca23a45c2d846a237712651f98b1a
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-09-19 20:37:04 +0300

    17.1 Сортировка

--- src/main/java/com/collection/list/ArrayListAdding.java
+++ src/main/java/com/collection/list/ArrayListAdding.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ArrayListAdding {
-
     public static void main(String[] args) {
         List<String> cities = new ArrayList<>();
         cities.add("Москва");
