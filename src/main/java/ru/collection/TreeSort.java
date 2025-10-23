f317f8b8fce5d81e2b190992f06e3e5af8e2fe6e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-10-23 19:54:20 +0300

    17.2. Организовать сортировку User

---
+++ src/main/java/ru/collection/TreeSort.java
@@ -0,0 +1,13 @@
+package ru.collection;
+
+import java.util.*;
+
+public class TreeSort {
+    public static void main(String[] args) {
+        Set<Integer> numbers = new TreeSet<>();
+        numbers.add(5);
+        numbers.add(1);
+        numbers.add(3);
+        System.out.println(numbers);
+    }
+}
