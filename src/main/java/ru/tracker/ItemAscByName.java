e0cba4fb80774afcb2e6a9f5da222343f04dfe2d
Valk <kravchenkoval@mail.ru>
2025-10-23 16:54:21 +0300

    17.1 Сортировка

---
+++ src/main/java/ru/tracker/ItemAscByName.java
@@ -0,0 +1,10 @@
+package ru.tracker;
+
+import java.util.Comparator;
+
+public class ItemAscByName implements Comparator<Item> {
+    @Override
+    public int compare(Item left, Item right) {
+        return left.getName().compareTo(right.getName());
+    }
+}
