034932605ad4b3a3011768a9f1475990b2de4ffa
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-10-23 20:56:32 +0300

    17.2. Организовать сортировку User

---
+++ src/main/java/ru/collection/User.java
@@ -0,0 +1,36 @@
+package ru.collection;
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        int nameCompare = this.name.compareTo(o.name);
+        if (nameCompare != 0) {
+            return nameCompare;
+        }
+        return Integer.compare(this.age, o.age);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return age == user.age && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
