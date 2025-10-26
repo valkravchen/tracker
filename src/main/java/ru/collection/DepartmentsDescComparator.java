4176baf7d6ce5b649dce5ae5afc9b4749df216ba
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-10-26 20:13:26 +0300

    19.1 Отсортировать департаменты

--- src/main/java/ru/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/collection/DepartmentsDescComparator.java
@@ -8,9 +8,6 @@ public class DepartmentsDescComparator implements Comparator<String> {
         String[] departments1 = o1.split("/");
         String[] departments2 = o2.split("/");
         int result = departments2[0].compareTo(departments1[0]);
-        if (result == 0) {
-            result = o1.compareTo(o2);
-        }
-        return result;
+        return (result != 0) ? result : o1.compareTo(o2);
     }
 }
