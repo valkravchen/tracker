5412031f4c27b2a8ef67a714ee25fe6dfa83d182
Valk <kravchenkoval@mail.ru>
2025-10-28 08:49:50 +0300

    20.5 Встроенные функциональные интерфейсы

--- src/main/java/ru/function/StrategyUsage.java
+++ src/main/java/ru/function/StrategyUsage.java
@@ -1,5 +1,6 @@
 package ru.function;
 
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class StrategyUsage {
@@ -19,16 +20,29 @@ public class StrategyUsage {
         return predicate.test(string);
     }
 
+    public String transform(Function<String, String> function, String string) {
+        return function.apply(string);
+    }
+
     public static void main(String[] args) {
         StrategyUsage usage = new StrategyUsage();
         System.out.println("Результат работы: " + usage.check(
-                string -> string.isEmpty(),  ""
+                string -> string.isEmpty(), ""
         ));
         System.out.println("Результат работы: " + usage.check(
                 string -> string.startsWith("Fun"), "Functional interface"
         ));
         System.out.println("Результат работы: " + usage.check(
                 string -> string.contains("rn"), "Surname Name"
+        ));
+        System.out.println("Строка после преобразования: " + usage.transform(
+                string -> string.toUpperCase(), "aBCdEfghKLmnpRstU"
+        ));
+        System.out.println(usage.transform(
+                string -> string.concat("работает корректно."), "Строка после преобразования: "
+        ));
+        System.out.println("Строка после преобразования: " + usage.transform(
+                string -> string.trim(), "    aBC dEfghK Lmnp RstU        "
         ));
     }
 }
