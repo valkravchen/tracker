13e79a522277b82c5107c2feab6dcec691dfa4eb
Valk <kravchenkoval@mail.ru>
2025-10-30 15:49:51 +0300

    20.11 Функции высшего порядка

---
+++ src/test/java/ru/stream/StreamUsage.java
@@ -0,0 +1,45 @@
+package ru.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamUsage {
+    public static class Task {
+        private final String name;
+        private final long spent;
+
+        public Task(String name, long spent) {
+            this.name = name;
+            this.spent = spent;
+        }
+
+        @Override
+        public String toString() {
+            return "Task{" +
+                    "name='" + name + '\'' +
+                    ", spent=" + spent +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Task> tasks = List.of(
+                new Task("Bug #1", 100),
+                new Task("Task #2", 100),
+                new Task("Bug #3", 100)
+        );
+        List<Task> bugs = tasks.stream().filter(
+                task -> task.name.contains("Bug")
+        ).collect(Collectors.toList());
+
+        List<String> names = tasks.stream().map(
+                task -> task.name
+        ).collect(Collectors.toList());
+        long total = tasks.stream().map(
+                task -> task.spent
+        ).reduce(0L, Long:: sum);
+        System.out.println(total);
+        bugs.forEach(System.out::println);
+        names.forEach(System.out::println);
+    }
+}
