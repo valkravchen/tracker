7cb142ba64457b72363008eefd280766c518b419
Valk <kravchenkoval@mail.ru>
2025-10-31 12:42:55 +0300

    21.5 Фильтрация учеников

---
+++ src/main/java/ru/stream/School.java
@@ -0,0 +1,12 @@
+package ru.stream;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class School {
+    public List<Student> collect(List<Student> students, Predicate<Student> predicate) {
+        return students.stream()
+                .filter(predicate)
+                .toList();
+    }
+}
