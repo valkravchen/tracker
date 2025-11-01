bcf468291d5c918bc722ee607d942cf85a5420a6
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-11-01 19:18:51 +0300

    21.11 Выборка элементов из потока

--- src/main/java/ru/stream/SelectionExample.java
+++ src/main/java/ru/stream/SelectionExample.java
@@ -5,11 +5,11 @@ import java.util.*;
 public class SelectionExample {
     public static void main(String[] args) {
         List<String> strings = Arrays.asList("Один", "Два", "Три", "Четыре", "Пять");
-        List<String> result = strings
+        List<String> result = Collections.singletonList(strings
                 .stream()
-                .skip(2)
-                .limit(2)
-                .toList();
+                .skip(5)
+                .findFirst()
+                .orElse("По умолчанию"));
         System.out.println(result);
     }
 }
