fe3f7364af0203cc3054dbe4ec0c2c3b628053e5
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-11-02 18:39:48 +0300

    21.13 Агрегатные методы min, max, count, average, sum

--- src/main/java/ru/stream/MinExample.java
+++ src/main/java/ru/stream/MinExample.java
@@ -7,7 +7,7 @@ public class MinExample {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(4, 5, 1, 3, 2);
         Optional<Integer> minEl = list.stream()
-                        .min(Comparator.naturalOrder());
+                .min(Comparator.naturalOrder());
         System.out.println(minEl.get());
         OptionalInt min = IntStream
                 .rangeClosed(1, 5)
