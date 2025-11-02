83684597d7ebaa736068bf050ecd454cdc4de918
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-11-02 19:11:09 +0300

    21.13 Агрегатные методы min, max, count, average, sum

---
+++ src/main/java/ru/stream/mapto/MinExampleMain.java
@@ -0,0 +1,19 @@
+package ru.stream.mapto;
+
+import java.util.*;
+
+public class MinExampleMain {
+    public static void main(String[] args) {
+        List<Person> people = Arrays.asList(
+                new Person("Михаил", 35),
+                new Person("Ольга", 26),
+                new Person("Антон", 20),
+                new Person("Виктор", 16),
+                new Person("Анна", 29)
+        );
+        Optional<Person> youngestPerson = people.stream()
+                .min(Comparator.comparing(Person::getAge));
+        int minAge = youngestPerson.get().getAge();
+        System.out.println(minAge);
+    }
+}
