49fe7dcc1fab3605598bee4ac409c7e47f11c950
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-11-03 06:43:52 +0300

    21.14 Агрегатный метод reduce()

--- src/main/java/ru/stream/ReduceExample.java
+++ src/main/java/ru/stream/ReduceExample.java
@@ -5,8 +5,13 @@ import java.util.*;
 public class ReduceExample {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(1, 2, 3, 4);
-        Optional<Integer> sum = numbers.stream().
-                reduce(Integer::sum);
+        Optional<Integer> sum = numbers.stream()
+                        .reduce(Integer::sum);
         System.out.println(sum.get());
+
+        List<String> nums = List.of("Один", "Два", "Три");
+        Optional<String> sum1 = nums.stream()
+                .reduce((left, right) -> left + ", " + right);
+        System.out.println(sum1.get());
     }
 }
