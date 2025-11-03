3b0b9f4f5b15fb72e0fb29e9825780717d4b9f2e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-11-03 11:34:27 +0300

    21.15 Методы проверки элементов потока на соответствие условию

---
+++ src/main/java/ru/stream/MatchExample.java
@@ -0,0 +1,13 @@
+package ru.stream;
+
+import java.util.*;
+
+public class MatchExample {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("Один", "Два", "Три", "Четыре", "Пять");
+        boolean result = strings
+                .stream()
+                .noneMatch("Шесть"::contains);
+        System.out.println(result);
+    }
+}
