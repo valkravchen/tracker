3e0c817c7cea11a50bb70e0d6f0056ea68d9d736
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-11-04 13:17:26 +0300

    22.1 Stream API. takeWhile

--- src/main/java/ru/api/TakeWhile.java
+++ src/main/java/ru/api/TakeWhile.java
@@ -1,10 +1,11 @@
 package ru.api;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class TakeWhile {
     public static void main(String[] args) {
-        List.of(1, 2, 3, 4).stream()
+        Stream.of(1, 2, 3, 4)
                 .takeWhile(value -> value < 3)
                 .map(value -> "Результат: " + value)
                 .forEach(System.out::println);
