84947450a1b2be3bdbe5f2e6fe2ffba23378b08d
Valk <kravchenkoval@mail.ru>
2025-11-05 16:26:42 +0300

    22.3 Local-Variable Type Inference

--- src/main/java/ru/map/College.java
+++ src/main/java/ru/map/College.java
@@ -9,38 +9,18 @@ public class College {
         this.students = students;
     }
 
-    public Student findByAccount(String account) {
+    public Optional<Student> findByAccount(String account) {
         return students.keySet()
                 .stream()
                 .filter(student -> student.account().equals(account))
-                .findFirst()
-                .orElse(null);
+                .findFirst();
     }
 
-    public Subject findBySubjectName(String account, String name) {
-        Student student = findByAccount(account);
-        if (student == null) {
-            return null;
-        }
-        return students.get(student)
+    public Optional<Subject> findBySubjectName(String account, String name) {
+        return findByAccount(account)
+                .flatMap(student -> students.get(student)
                 .stream()
                 .filter(subject -> subject.name().equals(name))
-                .findFirst()
-                .orElse(null);
-    }
-
-    public static void main(String[] args) {
-        Map<Student, Set<Subject>> students = Map.of(new Student("Student", "000001",
-                        "201-18-15"),
-                Set.of(
-                        new Subject("Math", 70),
-                        new Subject("English", 85)
-                )
-        );
-        College college = new College(students);
-        Student student = college.findByAccount("000001");
-        System.out.println("Найденный студент: " + student);
-        Subject english = college.findBySubjectName("000001", "English");
-        System.out.println("Оценка по найденному предмету: " + english.score());
+                .findFirst());
     }
 }
